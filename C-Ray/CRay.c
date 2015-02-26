09a86a4ac1f826405eb8d3a7923f7597f27d64dc
Valtteri Koskivuori <vkoskiv@gmail.com>
2015-02-26 15:28:01 +0200

    - Xcode

---
+++ C-Ray/CRay.c
@@ -0,0 +1,359 @@
+//
+//  CRay.c
+//  
+//
+//  Created by Valtteri Koskivuori on 12/02/15.
+//
+//
+/*
+ TODO:
+ Add input file
+ Add ambient color
+ Add camera object
+ */
+
+#include "CRay.h"
+
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+#define invsqrtf(x) (1.0f / sqrtf(x))
+
+//Image dimensions. Eventually get this from the input file
+#define kImgWidth 2560
+#define kImgHeight 1600
+#define kFrameCount 1
+#define bounces 2
+#define contrast 1.0
+
+//Global variables
+unsigned char *imgData = NULL;
+
+int main(int argc, char *argv[]) {
+	
+	time_t start, stop;
+	
+	//Seed RNGs
+	srand(time(NULL));
+	srand48(time(NULL));
+	
+	//Animation
+	for (int currentFrame = 0; currentFrame < kFrameCount; currentFrame++) {
+		//This is a timer to elapse how long a render takes
+		time(&start);
+		printf("Rendering at %i x %i\n",kImgWidth,kImgHeight);
+		printf("Building scene\n");
+		//Define the scene. Eventually read this from an input file
+		lightRay incidentRay;
+		
+		world background;
+		
+		background.ambientColor.red = 0.2;
+		background.ambientColor.green = 0.2;
+		background.ambientColor.blue = 0.2;
+		
+		//define materials to an array
+		material materials[5];
+		
+		materials[0].diffuse.red = 1;
+		materials[0].diffuse.green = 0;
+		materials[0].diffuse.blue = 0;
+		materials[0].reflectivity = 0.2;
+		
+		materials[1].diffuse.red = 0;
+		materials[1].diffuse.green = 1;
+		materials[1].diffuse.blue = 0;
+		materials[1].reflectivity = 0.5;
+		
+		materials[2].diffuse.red = 0;
+		materials[2].diffuse.green = 0;
+		materials[2].diffuse.blue = 1;
+		materials[2].reflectivity = 1;
+		
+		materials[3].diffuse.red = 0.3;
+		materials[3].diffuse.green = 0.3;
+		materials[3].diffuse.blue = 0.3;
+		materials[3].reflectivity = 1;
+		
+		materials[4].diffuse.red = 1;
+		materials[4].diffuse.green = 0.517647;
+		materials[4].diffuse.blue = 0;
+		materials[4].reflectivity = 1;
+		
+		//Define polygons to an array
+		
+		polygonObject polys[1];
+		
+		polys[0].v1.x = 200;
+		polys[0].v1.y = 50;
+		polys[0].v1.z = 0;
+		polys[0].v2.x = kImgWidth-200;
+		polys[0].v2.y = 50;
+		polys[0].v2.z = 0;
+		polys[0].v3.x = kImgWidth/2;
+		polys[0].v3.y = kImgHeight/2;
+		polys[0].v3.z = 2000;
+		polys[0].material = 4;
+		
+		//define spheres to an array
+		//Red sphere
+		sphereObject spheres[4];
+		spheres[0].pos.x = 400;
+		spheres[0].pos.y = 350;
+		spheres[0].pos.z = 0;
+		spheres[0].radius = 200;
+		spheres[0].material = 0;
+		
+		//green sphere
+		spheres[1].pos.x = 650;
+		spheres[1].pos.y = 800;
+		spheres[1].pos.z = 0;
+		spheres[1].radius = 150;
+		spheres[1].material = 1;
+		
+		//blue sphere
+		spheres[2].pos.x = 1000;
+		spheres[2].pos.y = 550;
+		spheres[2].pos.z = 0;
+		spheres[2].radius = 220;
+		spheres[2].material = 2;
+		
+		//grey sphere
+		spheres[3].pos.x = 770;
+		spheres[3].pos.y = 240;
+		spheres[3].pos.z = 400;
+		spheres[3].radius = 100;
+		spheres[3].material = 3;
+		
+		//Define random spheres to an array
+		/*int amount = rand()%50+50;
+		 printf("Spheres: %i\n",amount);
+		 int i;
+		 material materials[amount];
+		 sphereObject spheres[amount];
+		 
+		 for (i = 0; i < amount; i++) {
+			materials[i].diffuse.red = randRange(0,1);
+			materials[i].diffuse.green = randRange(0,1);
+			materials[i].diffuse.blue = randRange(0,1);
+			materials[i].reflectivity = randRange(0,1);
+			
+			spheres[i].pos.x = rand()%kImgWidth;
+			spheres[i].pos.y = rand()%kImgWidth;
+			spheres[i].pos.z = rand()&4000-2000;
+			spheres[i].radius = rand()%100+50;
+			spheres[i].material = i;
+		 }*/
+		
+		
+		//Define lights to an array
+		lightSource lights[3];
+		
+		lights[0].pos.x = 300;
+		lights[0].pos.y = 240;
+		lights[0].pos.z = -1000;
+		lights[0].intensity.red = 0.9;
+		lights[0].intensity.green = 0.9;
+		lights[0].intensity.blue = 0.9;
+		
+		lights[1].pos.x = 1280;
+		lights[1].pos.y = 3000;
+		lights[1].pos.z = -1000;
+		lights[1].intensity.red = 0.4;
+		lights[1].intensity.green = 0.4;
+		lights[1].intensity.blue = 0.4;
+		
+		lights[2].pos.x = 1600;
+		lights[2].pos.y = 1000;
+		lights[2].pos.z = -1000;
+		lights[2].intensity.red = 0.4;
+		lights[2].intensity.green = 0.4;
+		lights[2].intensity.blue = 0.4;
+		
+		printf("Using %i light bounces\n",bounces);
+		printf("Raytracing...\n");
+		//Create array of pixels for image data
+		//Allocate memory for it, remember to free!
+		if (imgData) {
+			free(imgData);
+		}
+		imgData = (unsigned char*)malloc(3*kImgWidth*kImgHeight);
+		memset(imgData, 0, 3*kImgWidth*kImgHeight);
+		//unsigned char imgData[3*kImgWidth*kImgHeight];
+		
+		//Start filling array with image data with ray tracing
+		int x, y;
+		
+		for (y = 0; y < kImgHeight; y++) {
+			for (x = 0; x < kImgWidth; x++) {
+				
+				color output = {0.0f, 0.0f, 0.0f};
+				
+				int level = 0;
+				double coefficient = contrast;
+				
+				//Camera position (Kind of, no real camera yet)
+				incidentRay.start.x = x;
+				incidentRay.start.y = y;
+				incidentRay.start.z = -2000;
+				
+				incidentRay.direction.x = 0;
+				incidentRay.direction.y = 0;
+				incidentRay.direction.z = 1;
+				
+				do {
+					
+					//Find the closest intersection first
+					double t = 20000.0f;
+					double temp;
+					int currentSphere = -1;
+					int currentPolygon = -1;
+					int sphereAmount = sizeof(spheres)/sizeof(sphereObject);
+					int polygonAmount = sizeof(polys)/sizeof(polygonObject);
+					int lightSourceAmount = sizeof(lights)/sizeof(lightSource);
+					
+					material currentMaterial;
+					vector polyNormal, hitpoint, surfaceNormal;
+					
+					unsigned int i;
+					for (i = 0; i < sphereAmount; i++) {
+						if (rayIntersectsWithSphere(&incidentRay, &spheres[i], &t)) {
+							currentSphere = i;
+						}
+					}
+					
+					for (i = 0; i < polygonAmount; i++) {
+						if (rayIntersectsWithPolygon(&incidentRay, &polys[i], &t, &polyNormal)) {
+							currentPolygon = i;
+							currentSphere = -1;
+						}
+					}
+					
+					//Ray-object intersection detection
+					if (currentSphere != -1) {
+						vector scaled = vectorScale(t, &incidentRay.direction);
+						hitpoint = addVectors(&incidentRay.start, &scaled);
+						surfaceNormal = subtractVectors(&hitpoint, &spheres[currentSphere].pos);
+						temp = scalarProduct(&surfaceNormal,&surfaceNormal);
+						if (temp == 0.0f) break;
+						temp = invsqrtf(temp); //Try fastInvSqrt for this :D
+						surfaceNormal = vectorScale(temp, &surfaceNormal);
+						currentMaterial = materials[spheres[currentSphere].material];
+					} else if (currentPolygon != -1) {
+						vector scaled = vectorScale(t, &incidentRay.direction);
+						hitpoint = addVectors(&incidentRay.start, &scaled);
+						surfaceNormal = polyNormal;
+						temp = scalarProduct(&surfaceNormal,&surfaceNormal);
+						if (temp == 0.0f) break;
+						temp = invsqrtf(temp);
+						surfaceNormal = vectorScale(temp, &surfaceNormal);
+						currentMaterial = materials[polys[currentPolygon].material];
+					} else {
+						//Ray didn't hit any object, set color to ambient
+						color back;
+						back.red = background.ambientColor.red;
+						back.green = background.ambientColor.green;
+						back.blue = background.ambientColor.blue;
+						color temp = colorCoef(coefficient, &back);
+						output = addColors(&output, &temp);
+						break;
+					}
+					
+					bool isInside;
+					
+					if (scalarProduct(&surfaceNormal, &incidentRay.direction) > 0.0f) {
+						surfaceNormal = vectorScale(-1.0f,&surfaceNormal);
+						isInside = true;
+					} else {
+						isInside = false;
+					}
+
+					if (!isInside) {
+						
+						lightRay bouncedRay;
+						bouncedRay.start = hitpoint;
+						//Find the value of the light at this point (Scary!)
+						unsigned int j;
+						for (j = 0; j < lightSourceAmount; j++) {
+							lightSource currentLight = lights[j];
+							bouncedRay.direction = subtractVectors(&currentLight.pos, &hitpoint);
+							
+							double lightProjection = scalarProduct(&bouncedRay.direction, &surfaceNormal);
+							if (lightProjection <= 0.0f) continue;
+							
+							double lightDistance = scalarProduct(&bouncedRay.direction, &bouncedRay.direction);
+							double temp = lightDistance;
+							
+							if (temp == 0.0f) continue;
+							temp = invsqrtf(temp);
+							bouncedRay.direction = vectorScale(temp, &bouncedRay.direction);
+							lightProjection = temp * lightProjection;
+							
+							//Calculate shadows
+							bool inShadow = false;
+							double t = lightDistance;
+							unsigned int k;
+							for (k = 0; k < sphereAmount; ++k) {
+								if (rayIntersectsWithSphere(&bouncedRay, &spheres[k], &t)) {
+									inShadow = true;
+									break;
+								}
+							}
+							
+							for (k = 0; k < polygonAmount; ++k) {
+								if (rayIntersectsWithPolygon(&bouncedRay, &polys[k], &t, &polyNormal)) {
+									inShadow = true;
+									break;
+								}
+							}
+							if (!inShadow) {
+								//Calculate Lambert diffusion
+								float lambert = scalarProduct(&bouncedRay.direction, &surfaceNormal) * coefficient;
+								output.red += lambert * currentLight.intensity.red * currentMaterial.diffuse.red;
+								output.green += lambert * currentLight.intensity.green * currentMaterial.diffuse.green;
+								output.blue += lambert * currentLight.intensity.blue * currentMaterial.diffuse.blue;
+							}
+						}
+						//Iterate over the reflection
+						coefficient *= currentMaterial.reflectivity;
+						
+						//Calculate reflected ray start and direction
+						double reflect = 2.0f * scalarProduct(&incidentRay.direction, &surfaceNormal);
+						incidentRay.start = hitpoint;
+						vector temp = vectorScale(reflect, &surfaceNormal);
+						incidentRay.direction = subtractVectors(&incidentRay.direction, &temp);
+					}
+					
+					level++;
+					
+				} while ((coefficient > 0.0f) && (level < bounces));
+				
+				//Write pixel color channels to array
+				imgData[(x + y*kImgWidth)*3 + 0] = (unsigned char)min(  output.red*255.0f, 255.0f);
+				imgData[(x + y*kImgWidth)*3 + 1] = (unsigned char)min(output.green*255.0f, 255.0f);
+				imgData[(x + y*kImgWidth)*3 + 2] = (unsigned char)min( output.blue*255.0f, 255.0f);
+			}
+		}
+		printf("Saving result\n");
+		//Save image data to a file
+		//saveImageFromArray("rendered.ppm", imgData, kImgWidth, kImgHeight);
+		
+		//String manipulation is lovely in C
+		char buf[16];
+		sprintf(buf, "rendered_%d.bmp", currentFrame);
+		printf("%s\n", buf);
+		saveBmpFromArray(buf, imgData, kImgWidth, kImgHeight);
+		long bytes = 3*kImgWidth*kImgHeight;
+		long mBytes = (bytes / 1024) / 1024;
+		printf("Wrote %ld megabytes to file.\n",mBytes);
+		
+		if (imgData) {
+			free(imgData);
+			printf("Released image data array\n");
+		}
+		
+		time(&stop);
+		printf("Finished render in %.0f seconds.\n", difftime(stop, start));
+
+	}
+	return 0;
+}
+
---
+++ C-Ray/CRay.h
@@ -0,0 +1,286 @@
+//
+//  CRay.h
+//  
+//
+//  Created by Valtteri Koskivuori on 12/02/15.
+//
+//
+
+#ifndef ____CRay__
+#define ____CRay__
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h> //Need this for boolean data type
+#include <math.h>
+#include <string.h>
+
+//Vector
+typedef struct {
+	float x, y, z;
+}vector;
+
+//Object type
+typedef struct {
+	int type;
+}type;
+
+//Object, a sphere in this case
+typedef struct {
+	vector pos;
+	type objectType;
+	float radius;
+	int material;
+}sphereObject;
+
+typedef struct {
+	vector v1, v2, v3; //Three vertices
+	int material;
+}polygonObject;
+
+typedef struct { //Maybe use two polygons for this
+	vector v1, v2, v3, v4; //Four vertices
+	int material;
+}plane;
+
+//Object, a cube (WIP)
+typedef struct {
+	vector pos;
+	type objectType;
+	float edgeLength;
+	int material;
+}cubeObject;
+
+//Simulated light ray
+typedef struct {
+	vector start;
+	vector direction;
+}lightRay;
+
+//Color
+typedef struct {
+	float red, green, blue;
+}color;
+
+//World
+typedef struct {
+	color ambientColor;
+}world;
+
+//material
+typedef struct {
+	color diffuse;
+	float reflectivity;
+}material;
+
+//Light source
+typedef struct {
+	vector pos;
+	color intensity;
+}lightSource;
+
+/* Vector Functions */
+
+//Add two vectors and return the resulting vector
+vector addVectors(vector *v1, vector *v2) {
+	vector result = {v1->x + v2->x, v1->y + v2->y, v1->z + v2->z};
+	return result;
+}
+
+//Subtract two vectors and return the resulting vector
+vector subtractVectors(vector *v1, vector *v2) {
+	vector result = {v1->x - v2->x, v1->y - v2->y, v1->z - v2->z };
+	return result;
+}
+
+//Multiply two vectors and return the dot product
+float scalarProduct(vector *v1, vector *v2) {
+	return v1->x * v2->x + v1->y * v2->y + v1->z * v2->z;
+}
+
+//Multiply a vector by a scalar and return the resulting vector
+vector vectorScale(double c, vector *v) {
+	vector result = {v->x * c, v->y * c, v->z * c};
+	return result;
+}
+
+//Calculate cross product and return the resulting vector
+vector vectorCross(vector *v1, vector *v2) {
+	vector result;
+	
+	result.x = (v1->y * v2->z) - (v1->z * v2->y);
+	result.y = (v1->z * v2->x) - (v1->x * v2->z);
+	result.z = (v1->x * v2->y) - (v1->y * v2->x);
+	
+	return result;
+}
+
+//Color functions
+//Multiply two colors
+color multiplyColors(color *c1, color *c2) {
+	color result = {c1->red * c2->red, c1->green * c2->green, c1->blue * c2->blue};
+	return result;
+}
+
+//Add two colors
+color addColors(color *c1, color *c2) {
+	color result = {c1->red + c2->red, c1->green + c2->green, c1->blue + c2->blue};
+	return result;
+}
+
+//Multiply a color with a coefficient value
+color colorCoef(double coef, color *c) {
+	color result = {c->red * coef, c->green * coef, c->blue * coef};
+	return result;
+}
+
+//Calculates intersection with a sphere and a light ray
+bool rayIntersectsWithSphere(lightRay *r, sphereObject *s, double *t) {
+	bool intersects = false;
+	
+	//Vector dot product of the direction
+	float A = scalarProduct(&r->direction, &r->direction);
+	
+	//Distance between start of a lightRay and the sphere position
+	vector distance = subtractVectors(&r->start, &s->pos);
+	
+	float B = 2 * scalarProduct(&r->direction, &distance);
+	
+	float C = scalarProduct(&distance, &distance) - (s->radius * s->radius);
+	
+	float trigDiscriminant = B * B - 4 * A * C;
+	
+	//If discriminant is negative, no real roots and the ray has missed the sphere
+	if (trigDiscriminant < 0) {
+		intersects = false;
+	} else {
+		float sqrtOfDiscriminant = sqrtf(trigDiscriminant);
+		float t0 = (-B + sqrtOfDiscriminant)/(2);
+		float t1 = (-B - sqrtOfDiscriminant)/(2);
+		
+		//Pick closest intersection
+		if (t0 > t1) {
+			t0 = t1;
+		}
+		
+		//Verify intersection is larger than 0 and less than the original distance
+		if ((t0 > 0.001f) && (t0 < *t)) {
+			*t = t0;
+			intersects = true;
+		} else {
+			intersects = false;
+		}
+	}
+	return intersects;
+}
+
+bool rayIntersectsWithPolygon(lightRay *r, polygonObject *t, double *result, vector *normal) {
+	double det, invdet;
+	vector edge1 = subtractVectors(&t->v2, &t->v1);
+	vector edge2 = subtractVectors(&t->v3, &t->v1);
+	
+	//Find the cross product of edge 2 and the current ray direction
+	vector s1 = vectorCross(&r->direction, &edge2);
+	
+	det = scalarProduct(&edge1, &s1);
+	//Prepare for floating point precision errors, find a better way to fix these!
+	if (det > -0.000001 && det < 0.000001) {
+		return false;
+	}
+	
+	invdet = 1/det;
+	
+	vector s2 = subtractVectors(&r->start, &t->v1);
+	double u = scalarProduct(&s2, &s1) * invdet;
+	if (u < 0 || u > 1) {
+		return false;
+	}
+	
+	vector s3 = vectorCross(&s2, &edge1);
+	double v = scalarProduct(&r->direction, &s3) * invdet;
+	if (v < 0 || (u+v) > 1) {
+		return false;
+	}
+	
+	double temp = scalarProduct(&edge2, &s3) * invdet;
+	
+	if (((temp < 0) || (temp > *result))) {
+		return false;
+	}
+		
+		*result = temp - 0.005;
+		*normal = vectorCross(&edge2, &edge1);
+		
+		return true;
+}
+
+void saveImageFromArray(char *filename, unsigned char *imgdata, int width, int height) {
+	//File pointer
+	FILE *f;
+	//Open the file
+	f = fopen(filename, "w");
+	//Write the PPM format header info
+	fprintf(f, "P6 %d %d %d\n", width, height, 255);
+	//Write given image data to the file, 3 bytes/pixel
+	fwrite(imgdata, 3, width*height, f);
+	//Close the file
+	fclose(f);
+}
+
+void saveBmpFromArray(char *filename, unsigned char *imgData, int width, int height) {
+	int i;
+	int error;
+	FILE *f;
+	int filesize = 54 + 3*width*height;
+	
+	unsigned char bmpfileheader[14] = {'B','M', 0,0,0,0, 0,0, 0,0, 54,0,0,0};
+	unsigned char bmpinfoheader[40] = {40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0, 24,0};
+	unsigned char bmppadding[3] = {0,0,0};
+	
+	//Create header with filesize data
+	bmpfileheader[2] = (unsigned char)(filesize    );
+	bmpfileheader[3] = (unsigned char)(filesize>> 8);
+	bmpfileheader[4] = (unsigned char)(filesize>>16);
+	bmpfileheader[5] = (unsigned char)(filesize>>24);
+	
+	//create header width and height info
+	bmpinfoheader[ 4] = (unsigned char)(width    );
+	bmpinfoheader[ 5] = (unsigned char)(width>>8 );
+	bmpinfoheader[ 6] = (unsigned char)(width>>16);
+	bmpinfoheader[ 7] = (unsigned char)(width>>24);
+	
+	bmpinfoheader[ 8] = (unsigned char)(height    );
+	bmpinfoheader[ 9] = (unsigned char)(height>>8 );
+	bmpinfoheader[10] = (unsigned char)(height>>16);
+	bmpinfoheader[11] = (unsigned char)(height>>24);
+	
+	f = fopen(filename,"wb");
+	error = fwrite(bmpfileheader,1,14,f);
+	if (error != 14) {
+		printf("Error writing BMP file header data\n");
+	}
+	error = fwrite(bmpinfoheader,1,40,f);
+	if (error != 40) {
+		printf("Error writing BMP info header data\n");
+	}
+	
+	for (i = 0; i < height; i++) {
+		error = fwrite(imgData+(width*(i)*3),3,width,f);
+		if (error != width) {
+			printf("Error writing image line to BMP\n");
+		}
+		error = fwrite(bmppadding,1,(4-(width*3)%4)%4,f);
+		if (error != (4-(width*3)%4)%4) {
+			printf("Error writing BMP padding data\n");
+		}
+	}
+	fclose(f);
+}
+
+float randRange(float a, float b)
+{
+	return ((b-a)*((float)rand()/RAND_MAX))+a;
+}
+
+#endif /* defined(____CRay__) */
