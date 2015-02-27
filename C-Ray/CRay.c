8b452b6cbfefb167d5fa49292f5c04fa6da43812
Valtteri Koskivuori <vkoskiv@gmail.com>
2015-02-27 18:39:54 +0200

    - Redid world building to make implementing a tokenizer easier - Much cleaner now :D - Hope there's no memory leaks

--- C-Ray/CRay.c
+++ C-Ray/CRay.c
@@ -17,17 +17,9 @@
 #define min(a,b) (((a) < (b)) ? (a) : (b))
 #define invsqrtf(x) (1.0f / sqrtf(x))
 
-//Image dimensions. Eventually get this from the input file
-#define kImgWidth 1920
-#define kImgHeight 1080
-#define kFrameCount 1
-#define bounces 2
-#define contrast 1.0
-
 //Global variables
 unsigned char *imgData = NULL;
 unsigned long bounceCount = 0;
-randomGenerator = false;
 
 int main(int argc, char *argv[]) {
 	
@@ -46,187 +38,17 @@ int main(int argc, char *argv[]) {
 		//Define the scene. Eventually read this from an input file
 		lightRay incidentRay;
 		
-		world background;
-		
-		background.ambientColor.red =   0.41;
-		background.ambientColor.green = 0.41;
-		background.ambientColor.blue =  0.41;
-		
-		//define materials to an array
-		material materials[5];
-		
-		materials[0].diffuse.red = 1;
-		materials[0].diffuse.green = 0;
-		materials[0].diffuse.blue = 0;
-		materials[0].reflectivity = 0.2;
-		
-		materials[1].diffuse.red = 0;
-		materials[1].diffuse.green = 1;
-		materials[1].diffuse.blue = 0;
-		materials[1].reflectivity = 0.5;
-		
-		materials[2].diffuse.red = 0;
-		materials[2].diffuse.green = 0;
-		materials[2].diffuse.blue = 1;
-		materials[2].reflectivity = 1;
-		
-		materials[3].diffuse.red = 0.3;
-		materials[3].diffuse.green = 0.3;
-		materials[3].diffuse.blue = 0.3;
-		materials[3].reflectivity = 1;
-		
-		materials[4].diffuse.red = 0;
-		materials[4].diffuse.green = 0.517647;
-		materials[4].diffuse.blue = 1;
-		materials[4].reflectivity = 1;
-		
-		//Define polygons to an array
-		
-		//Backup
-		/*polys[0].v1.x = 200;
-		polys[0].v1.y = 50;
-		polys[0].v1.z = 0;
-		polys[0].v2.x = kImgWidth-200;
-		polys[0].v2.y = 50;
-		polys[0].v2.z = 0;
-		polys[0].v3.x = kImgWidth/2;
-		polys[0].v3.y = kImgHeight/2;
-		polys[0].v3.z = 2000;
-		polys[0].material = 0;*/
-		
-		polygonObject polys[4];
-		
-		//Bottom plane
-		polys[0].v1.x = 200;
-		polys[0].v1.y = 50;
-		polys[0].v1.z = 0;
-		
-		polys[0].v2.x = kImgWidth-200;
-		polys[0].v2.y = 50;
-		polys[0].v2.z = 0;
-		
-		polys[0].v3.x = 400;
-		polys[0].v3.y = kImgHeight/2;
-		polys[0].v3.z = 2000;
-		polys[0].material = 3;
-		
-		polys[1].v1.x = kImgWidth-200;
-		polys[1].v1.y = 50;
-		polys[1].v1.z = 0;
-		
-		polys[1].v2.x = kImgWidth-400;
-		polys[1].v2.y = kImgHeight/2;
-		polys[1].v2.z = 2000;
-		
-		polys[1].v3.x = 400;
-		polys[1].v3.y = kImgHeight/2;
-		polys[1].v3.z = 2000;
-		polys[1].material = 3;
-		
-		//Background plane
-		//First poly
-		//bottom left
-		polys[2].v1.x = 400;
-		polys[2].v1.y = kImgHeight/2;
-		polys[2].v1.z = 2020;
-		//Bottom right
-		polys[2].v2.x = kImgWidth-400;
-		polys[2].v2.y = kImgHeight/2;
-		polys[2].v2.z = 2020;
-		//top left
-		polys[2].v3.x = 400;
-		polys[2].v3.y = kImgHeight;
-		polys[2].v3.z = 1500;
-		polys[2].material = 3;
-		//Second poly
-		//top right
-		polys[3].v1.x = kImgWidth-400;
-		polys[3].v1.y = kImgHeight;
-		polys[3].v1.z = 1500;
-		//top left
-		polys[3].v2.x = 400;
-		polys[3].v2.y = kImgHeight;
-		polys[3].v2.z = 1500;
-		//bottom right
-		polys[3].v3.x = kImgWidth-400;
-		polys[3].v3.y = kImgHeight/2;
-		polys[3].v3.z = 2020;
-		polys[3].material = 3;
-		
-		//define spheres to an array
-		//Red sphere
-		sphereObject spheres[4];
-		spheres[0].pos.x = 400;
-		spheres[0].pos.y = 260;
-		spheres[0].pos.z = 0;
-		spheres[0].radius = 200;
-		spheres[0].material = 0;
-		
-		//green sphere
-		spheres[1].pos.x = 650;
-		spheres[1].pos.y = 630;
-		spheres[1].pos.z = 1750;
-		spheres[1].radius = 150;
-		spheres[1].material = 1;
-		
-		//blue sphere
-		spheres[2].pos.x = 1300;
-		spheres[2].pos.y = 520;
-		spheres[2].pos.z = 1000;
-		spheres[2].radius = 220;
-		spheres[2].material = 2;
-		
-		//grey sphere
-		spheres[3].pos.x = 970;
-		spheres[3].pos.y = 250;
-		spheres[3].pos.z = 400;
-		spheres[3].radius = 100;
-		spheres[3].material = 3;
-		
-		//Define lights to an array
-		lightSource lights[1];
-		
-		lights[0].pos.x = kImgWidth/2;
-		lights[0].pos.y = 1080-100;
-		lights[0].pos.z = 0;
-		lights[0].intensity.red = 0.9;
-		lights[0].intensity.green = 0.9;
-		lights[0].intensity.blue = 0.9;
-		
-		/*lights[1].pos.x = 1280;
-		lights[1].pos.y = 3000;
-		lights[1].pos.z = -1000;
-		lights[1].intensity.red = 0.4;
-		lights[1].intensity.green = 0.4;
-		lights[1].intensity.blue = 0.4;
-		
-		lights[2].pos.x = 1600;
-		lights[2].pos.y = 1000;
-		lights[2].pos.z = -1000;
-		lights[2].intensity.red = 0.4;
-		lights[2].intensity.green = 0.4;
-		lights[2].intensity.blue = 0.4;*/
-		
-		if (randomGenerator) {
-			//Define random scene
-			int amount = rand()%50+50;
-			printf("Spheres: %i\n",amount);
-			int i;
-			material materials[amount];
-			sphereObject spheres[amount];
-		 
-		 for (i = 0; i < amount; i++) {
-			 materials[i].diffuse.red = randRange(0,1);
-			 materials[i].diffuse.green = randRange(0,1);
-			 materials[i].diffuse.blue = randRange(0,1);
-			 materials[i].reflectivity = randRange(0,1);
-			 
-			 spheres[i].pos.x = rand()%kImgWidth;
-			 spheres[i].pos.y = rand()%kImgWidth;
-			 spheres[i].pos.z = rand()&4000-2000;
-			 spheres[i].radius = rand()%100+50;
-			 spheres[i].material = i;
-		 }
+		//Create the scene
+		world sceneObject;
+		sceneObject.materials = NULL;
+		sceneObject.spheres = NULL;
+		sceneObject.polys = NULL;
+		sceneObject.lights = NULL;
+		world *worldScene = NULL;
+		worldScene = &sceneObject;
+		//Build the scene
+		if (buildScene(false, worldScene) == -1) {
+			printf("Error building worldScene\n");
 		}
 		
 		printf("Using %i light bounces\n",bounces);
@@ -238,7 +60,6 @@ int main(int argc, char *argv[]) {
 		}
 		imgData = (unsigned char*)malloc(3*kImgWidth*kImgHeight);
 		memset(imgData, 0, 3*kImgWidth*kImgHeight);
-		//unsigned char imgData[3*kImgWidth*kImgHeight];
 		
 		//Start filling array with image data with ray tracing
 		int x, y;
@@ -267,22 +88,22 @@ int main(int argc, char *argv[]) {
 					double temp;
 					int currentSphere = -1;
 					int currentPolygon = -1;
-					int sphereAmount = (unsigned int)sizeof(spheres)/sizeof(sphereObject);
-					int polygonAmount = sizeof(polys)/sizeof(polygonObject);
-					int lightSourceAmount = sizeof(lights)/sizeof(lightSource);
+					int sphereAmount = worldScene->sphereAmount;
+					int polygonAmount = worldScene->polygonAmount;
+					int lightSourceAmount = worldScene->lightAmount;
 					
 					material currentMaterial;
 					vector polyNormal, hitpoint, surfaceNormal;
 					
 					unsigned int i;
 					for (i = 0; i < sphereAmount; i++) {
-						if (rayIntersectsWithSphere(&incidentRay, &spheres[i], &t)) {
+						if (rayIntersectsWithSphere(&incidentRay, &worldScene->spheres[i], &t)) {
 							currentSphere = i;
 						}
 					}
 					
 					for (i = 0; i < polygonAmount; i++) {
-						if (rayIntersectsWithPolygon(&incidentRay, &polys[i], &t, &polyNormal)) {
+						if (rayIntersectsWithPolygon(&incidentRay, &worldScene->polys[i], &t, &polyNormal)) {
 							currentPolygon = i;
 							currentSphere = -1;
 						}
@@ -293,12 +114,12 @@ int main(int argc, char *argv[]) {
 						bounceCount++;
 						vector scaled = vectorScale(t, &incidentRay.direction);
 						hitpoint = addVectors(&incidentRay.start, &scaled);
-						surfaceNormal = subtractVectors(&hitpoint, &spheres[currentSphere].pos);
+						surfaceNormal = subtractVectors(&hitpoint, &worldScene->spheres[currentSphere].pos);
 						temp = scalarProduct(&surfaceNormal,&surfaceNormal);
 						if (temp == 0.0f) break;
-						temp = FastInvSqrt(temp);
+						temp = invsqrtf(temp);
 						surfaceNormal = vectorScale(temp, &surfaceNormal);
-						currentMaterial = materials[spheres[currentSphere].material];
+						currentMaterial = worldScene->materials[worldScene->spheres[currentSphere].material];
 					} else if (currentPolygon != -1) {
 						bounceCount++;
 						vector scaled = vectorScale(t, &incidentRay.direction);
@@ -306,15 +127,15 @@ int main(int argc, char *argv[]) {
 						surfaceNormal = polyNormal;
 						temp = scalarProduct(&surfaceNormal,&surfaceNormal);
 						if (temp == 0.0f) break;
-						temp = FastInvSqrt(temp);
+						temp = invsqrtf(temp);
 						surfaceNormal = vectorScale(temp, &surfaceNormal);
-						currentMaterial = materials[polys[currentPolygon].material];
+						currentMaterial = worldScene->materials[worldScene->polys[currentPolygon].material];
 					} else {
 						//Ray didn't hit any object, set color to ambient
 						color back;
-						back.red = background.ambientColor.red;
-						back.green = background.ambientColor.green;
-						back.blue = background.ambientColor.blue;
+						back.red = worldScene->ambientColor->red;
+						back.green = worldScene->ambientColor->green;
+						back.blue = worldScene->ambientColor->blue;
 						color temp = colorCoef(coefficient, &back);
 						output = addColors(&output, &temp);
 						break;
@@ -336,7 +157,8 @@ int main(int argc, char *argv[]) {
 						//Find the value of the light at this point (Scary!)
 						unsigned int j;
 						for (j = 0; j < lightSourceAmount; j++) {
-							lightSource currentLight = lights[j];
+							//TODO: Implement rayIntersectsWithSphere here
+							lightSource currentLight = worldScene->lights[j];
 							bouncedRay.direction = subtractVectors(&currentLight.pos, &hitpoint);
 							
 							double lightProjection = scalarProduct(&bouncedRay.direction, &surfaceNormal);
@@ -355,14 +177,14 @@ int main(int argc, char *argv[]) {
 							double t = lightDistance;
 							unsigned int k;
 							for (k = 0; k < sphereAmount; ++k) {
-								if (rayIntersectsWithSphere(&bouncedRay, &spheres[k], &t)) {
+								if (rayIntersectsWithSphere(&bouncedRay, &worldScene->spheres[k], &t)) {
 									inShadow = true;
 									break;
 								}
 							}
 							
 							for (k = 0; k < polygonAmount; ++k) {
-								if (rayIntersectsWithPolygon(&bouncedRay, &polys[k], &t, &polyNormal)) {
+								if (rayIntersectsWithPolygon(&bouncedRay, &worldScene->polys[k], &t, &polyNormal)) {
 									inShadow = true;
 									break;
 								}
@@ -408,10 +230,19 @@ int main(int argc, char *argv[]) {
 		long mBytes = (bytes / 1024) / 1024;
 		printf("Wrote %ld megabytes to file.\n",mBytes);
 		
-		if (imgData) {
+		//Free memory
+		if (imgData)
 			free(imgData);
-			printf("Released image data array\n");
-		}
+		if (worldScene->lights)
+			free(worldScene->lights);
+		if (worldScene->spheres)
+			free(worldScene->spheres);
+		if (worldScene->polys)
+			free(worldScene->polys);
+		if (worldScene->materials)
+			free(worldScene->materials);
+		
+		
 		time(&stop);
 		printf("Finished render in %.0f seconds.\n", difftime(stop, start));
 
--- C-Ray/CRay.h
+++ C-Ray/CRay.h
@@ -16,6 +16,13 @@
 #include <math.h>
 #include <string.h>
 
+//Image dimensions. Eventually get this from the input file
+#define kImgWidth 1920
+#define kImgHeight 1080
+#define kFrameCount 1
+#define bounces 2
+#define contrast 1.0
+
 //Vector
 typedef struct {
 	float x, y, z;
@@ -39,11 +46,6 @@ typedef struct {
 	int material;
 }polygonObject;
 
-typedef struct { //Maybe use two polygons for this
-	vector v1, v2, v3, v4; //Four vertices
-	int material;
-}plane;
-
 //Object, a cube (WIP)
 typedef struct {
 	vector pos;
@@ -63,11 +65,6 @@ typedef struct {
 	float red, green, blue;
 }color;
 
-//World
-typedef struct {
-	color ambientColor;
-}world;
-
 //material
 typedef struct {
 	color diffuse;
@@ -77,8 +74,25 @@ typedef struct {
 //Light source
 typedef struct {
 	vector pos;
+	float radius;
 	color intensity;
 }lightSource;
+
+//World
+typedef struct {
+	color *ambientColor;
+	lightSource *lights;
+	material *materials;
+	sphereObject *spheres;
+	polygonObject *polys;
+	
+	int height, width;
+	
+	int sphereAmount;
+	int polygonAmount;
+	int materialAmount;
+	int lightAmount;
+}world;
 
 /* Vector Functions */
 
@@ -133,6 +147,38 @@ color addColors(color *c1, color *c2) {
 color colorCoef(double coef, color *c) {
 	color result = {c->red * coef, c->green * coef, c->blue * coef};
 	return result;
+}
+
+bool rayIntersectsWithLight(lightRay *ray, lightSource *light, double *t) {
+	bool intersects = false;
+	
+	//Vector dot product of the direction
+	float A = scalarProduct(&ray->direction, &ray->direction);
+	//Distance between start of a bounced ray and the light pos
+	vector distance = subtractVectors(&ray->start, &light->pos);
+	float B = 2 * scalarProduct(&ray->direction, &distance);
+	float C = scalarProduct(&distance, &distance) - (light->radius * light->radius);
+	float trigDiscriminant = B * B - 4 * A * C;
+	
+	//If trigDiscriminant is negative, ray has missed the lightSource
+	if (trigDiscriminant < 0) {
+		intersects = false;
+	} else {
+		float sqrtOfDiscriminant = sqrtf(trigDiscriminant);
+		float t0 = (-B + sqrtOfDiscriminant)/(2);
+		float t1 = (-B - sqrtOfDiscriminant)/(2);
+		//Pick closest intersection
+		if (t0 > t1) {
+			t0 = t1;
+		}
+		if ((t0 > 0.001f) && (t0 < *t)) {
+			*t = t0;
+			intersects = true;
+		} else {
+			intersects = false;
+		}
+	}
+	return intersects;
 }
 
 //Calculates intersection with a sphere and a light ray
@@ -215,6 +261,196 @@ bool rayIntersectsWithPolygon(lightRay *r, polygonObject *t, double *result, vec
 		return true;
 }
 
+float randRange(float a, float b)
+{
+	return ((b-a)*((float)rand()/RAND_MAX))+a;
+}
+
+//TODO: Turn this into a proper tokenizer
+int buildScene(bool randomGenerator, world *scene) {
+	if (!randomGenerator) {
+		
+		scene->ambientColor = (color * )calloc(3, sizeof(color));
+		scene->ambientColor->red =   0.41;
+		scene->ambientColor->green = 0.41;
+		scene->ambientColor->blue =  0.41;
+		
+		//define materials to an array
+		scene->materialAmount = 6;
+		
+		scene->materials = (material *)calloc(scene->materialAmount, sizeof(material));
+		scene->materials[0].diffuse.red = 1;
+		scene->materials[0].diffuse.green = 0;
+		scene->materials[0].diffuse.blue = 0;
+		scene->materials[0].reflectivity = 0.2;
+		
+		scene->materials[1].diffuse.red = 0;
+		scene->materials[1].diffuse.green = 1;
+		scene->materials[1].diffuse.blue = 0;
+		scene->materials[1].reflectivity = 0.5;
+		
+		scene->materials[2].diffuse.red = 0;
+		scene->materials[2].diffuse.green = 0;
+		scene->materials[2].diffuse.blue = 1;
+		scene->materials[2].reflectivity = 1;
+		
+		scene->materials[3].diffuse.red = 0.3;
+		scene->materials[3].diffuse.green = 0.3;
+		scene->materials[3].diffuse.blue = 0.3;
+		scene->materials[3].reflectivity = 1;
+		
+		scene->materials[4].diffuse.red = 0;
+		scene->materials[4].diffuse.green = 0.517647;
+		scene->materials[4].diffuse.blue = 1;
+		scene->materials[4].reflectivity = 1;
+		
+		scene->materials[5].diffuse.red = 0.3;
+		scene->materials[5].diffuse.green = 0.3;
+		scene->materials[5].diffuse.blue = 0.3;
+		scene->materials[5].reflectivity = 0.5;
+		
+		//Define polygons to an array
+		scene->polygonAmount = 4;
+		
+		scene->polys = (polygonObject *)calloc(scene->polygonAmount, sizeof(polygonObject));
+		//Bottom plane
+		scene->polys[0].v1.x = 200;
+		scene->polys[0].v1.y = 50;
+		scene->polys[0].v1.z = 0;
+		
+		scene->polys[0].v2.x = kImgWidth-200;
+		scene->polys[0].v2.y = 50;
+		scene->polys[0].v2.z = 0;
+		
+		scene->polys[0].v3.x = 400;
+		scene->polys[0].v3.y = kImgHeight/2;
+		scene->polys[0].v3.z = 2000;
+		scene->polys[0].material = 3;
+		
+		scene->polys[1].v1.x = kImgWidth-200;
+		scene->polys[1].v1.y = 50;
+		scene->polys[1].v1.z = 0;
+		
+		scene->polys[1].v2.x = kImgWidth-400;
+		scene->polys[1].v2.y = kImgHeight/2;
+		scene->polys[1].v2.z = 2000;
+		
+		scene->polys[1].v3.x = 400;
+		scene->polys[1].v3.y = kImgHeight/2;
+		scene->polys[1].v3.z = 2000;
+		scene->polys[1].material = 3;
+		
+		//Background plane
+		//First poly
+		//bottom left
+		scene->polys[2].v1.x = 400;
+		scene->polys[2].v1.y = kImgHeight/2;
+		scene->polys[2].v1.z = 2020;
+		//Bottom right
+		scene->polys[2].v2.x = kImgWidth-400;
+		scene->polys[2].v2.y = kImgHeight/2;
+		scene->polys[2].v2.z = 2020;
+		//top left
+		scene->polys[2].v3.x = 400;
+		scene->polys[2].v3.y = kImgHeight;
+		scene->polys[2].v3.z = 1500;
+		scene->polys[2].material = 5;
+		//Second poly
+		//top right
+		scene->polys[3].v1.x = kImgWidth-400;
+		scene->polys[3].v1.y = kImgHeight;
+		scene->polys[3].v1.z = 1500;
+		//top left
+		scene->polys[3].v2.x = 400;
+		scene->polys[3].v2.y = kImgHeight;
+		scene->polys[3].v2.z = 1500;
+		//bottom right
+		scene->polys[3].v3.x = kImgWidth-400;
+		scene->polys[3].v3.y = kImgHeight/2;
+		scene->polys[3].v3.z = 2020;
+		scene->polys[3].material = 5;
+		
+		//define spheres to an array
+		//Red sphere
+		scene->sphereAmount = 4;
+		
+		scene->spheres = (sphereObject *)calloc(scene->sphereAmount, sizeof(sphereObject));
+		scene->spheres[0].pos.x = 400;
+		scene->spheres[0].pos.y = 260;
+		scene->spheres[0].pos.z = 0;
+		scene->spheres[0].radius = 200;
+		scene->spheres[0].material = 0;
+		
+		//green sphere
+		scene->spheres[1].pos.x = 650;
+		scene->spheres[1].pos.y = 630;
+		scene->spheres[1].pos.z = 1750;
+		scene->spheres[1].radius = 150;
+		scene->spheres[1].material = 1;
+		
+		//blue sphere
+		scene->spheres[2].pos.x = 1300;
+		scene->spheres[2].pos.y = 520;
+		scene->spheres[2].pos.z = 1000;
+		scene->spheres[2].radius = 220;
+		scene->spheres[2].material = 2;
+		
+		//grey sphere
+		scene->spheres[3].pos.x = 970;
+		scene->spheres[3].pos.y = 250;
+		scene->spheres[3].pos.z = 400;
+		scene->spheres[3].radius = 100;
+		scene->spheres[3].material = 3;
+		
+		//Define lights to an array
+		scene->lightAmount = 1;
+		
+		scene->lights = (lightSource *)calloc(scene->lightAmount, sizeof(lightSource));
+		scene->lights[0].pos.x = kImgWidth/2;
+		scene->lights[0].pos.y = 1080-100;
+		scene->lights[0].pos.z = 0;
+		scene->lights[0].intensity.red = 0.9;
+		scene->lights[0].intensity.green = 0.9;
+		scene->lights[0].intensity.blue = 0.9;
+		scene->lights[0].radius = 1.0;
+		
+		/*lights[1].pos.x = 1280;
+		 lights[1].pos.y = 3000;
+		 lights[1].pos.z = -1000;
+		 lights[1].intensity.red = 0.4;
+		 lights[1].intensity.green = 0.4;
+		 lights[1].intensity.blue = 0.4;
+		 
+		 lights[2].pos.x = 1600;
+		 lights[2].pos.y = 1000;
+		 lights[2].pos.z = -1000;
+		 lights[2].intensity.red = 0.4;
+		 lights[2].intensity.green = 0.4;
+		 lights[2].intensity.blue = 0.4;*/
+		
+		return 0;
+	} else {
+		//Define random scene
+		int amount = rand()%50+50;
+		printf("Spheres: %i\n",amount);
+		int i;
+		
+		for (i = 0; i < amount; i++) {
+			scene->materials[i].diffuse.red = randRange(0,1);
+			scene->materials[i].diffuse.green = randRange(0,1);
+			scene->materials[i].diffuse.blue = randRange(0,1);
+			scene->materials[i].reflectivity = randRange(0,1);
+			
+			scene->spheres[i].pos.x = rand()%kImgWidth;
+			scene->spheres[i].pos.y = rand()%kImgWidth;
+			scene->spheres[i].pos.z = rand()&4000-2000;
+			scene->spheres[i].radius = rand()%100+50;
+			scene->spheres[i].material = i;
+		}
+		return 0;
+	}
+}
+
 void saveImageFromArray(char *filename, unsigned char *imgdata, int width, int height) {
 	//File pointer
 	FILE *f;
@@ -276,11 +512,6 @@ void saveBmpFromArray(char *filename, unsigned char *imgData, int width, int hei
 		}
 	}
 	fclose(f);
-}
-
-float randRange(float a, float b)
-{
-	return ((b-a)*((float)rand()/RAND_MAX))+a;
 }
 
 float FastInvSqrt(float x) {
