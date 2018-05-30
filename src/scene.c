28a45550e4c34df2277ba7d8bef19f77d70ca0d3
Valtteri Koskivuori <vkoskiv@gmail.com>
2018-05-30 20:25:57 +0300

    Add cJSON to Windows build Remove `restrict` from portable getdelim()

--- src/scene.c
+++ src/scene.c
@@ -905,7 +905,7 @@ char *trimSpaces(char *inputLine) {
 #define	LONG_MAX	2147483647L	/* max signed long */
 #define	SSIZE_MAX	LONG_MAX	/* max value for a ssize_t */
 #define	EOVERFLOW	84		/* Value too large to be stored in data type */
-size_t getDelim(char **restrict lineptr, size_t *restrict n, int delimiter, FILE *restrict stream) {
+size_t getDelim(char **lineptr, size_t *restrict n, int delimiter, FILE *restrict stream) {
 	char *buf, *pos;
 	int c;
 	size_t bytes;
