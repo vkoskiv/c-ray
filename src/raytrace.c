vkoskiv/c-ray

commit 7a9758c2f4a0df2f8bc734aaeb9ec0f9d4c25160

Move contrast, bounces, areaLights from camera -> scene

src/raytrace.c
@@ -296,7 +296,7 @@ struct color getHighlights(const struct intersection *isect, struct color *color
 		struct light currentLight = scene->lights[i];
 		struct vector lightPos;
 		
-		if (scene->camera->areaLights)
+		if (scene->areaLights)
 			lightPos = getRandomVecOnRadius(currentLight.pos, currentLight.power);
 		else
 			lightPos = currentLight.pos;
@@ -500,7 +500,6 @@ struct color rayTrace(struct lightRay *incidentRay, struct world *scene) {
 	//Raytrace a given light ray with a given scene, then return the color value for that ray
 	struct color output = {0.0,0.0,0.0,0.0};
 	int bounces = 0;
-	double contrast = scene->camera->contrast;
 	
 	struct intersection *isectInfo = (struct intersection*)calloc(1, sizeof(struct intersection));
 	struct intersection *shadowInfo = (struct intersection*)calloc(1, sizeof(struct intersection));
@@ -573,7 +572,7 @@ struct color rayTrace(struct lightRay *incidentRay, struct world *scene) {
 			surfaceNormal = vectorScale(temp, &surfaceNormal);
 		} else {
 			//Ray didn't hit any object, set color to ambient
-			struct color temp = colorCoef(contrast, scene->ambientColor);
+			struct color temp = colorCoef(scene->contrast, scene->ambientColor);
 			output = addColors(&output, &temp);
 			break;
 		}
@@ -591,7 +590,7 @@ struct color rayTrace(struct lightRay *incidentRay, struct world *scene) {
 		for (unsigned j = 0; j < lightSourceAmount; ++j) {
 			struct light currentLight = scene->lights[j];
 			struct vector lightPos;
-			if (scene->camera->areaLights)
+			if (scene->areaLights)
 				lightPos = getRandomVecOnRadius(currentLight.pos, currentLight.power);
 			else
 				lightPos = currentLight.pos;
@@ -634,14 +633,14 @@ struct color rayTrace(struct lightRay *incidentRay, struct world *scene) {
 				double specularFactor = 1.0;//scalarProduct(&cameraRay.direction, &surfaceNormal) * contrast;
 				
 				//Calculate Lambert diffusion
-				double diffuseFactor = scalarProduct(&bouncedRay.direction, &surfaceNormal) * contrast;
+				double diffuseFactor = scalarProduct(&bouncedRay.direction, &surfaceNormal) * scene->contrast;
 				output.red += specularFactor * diffuseFactor * currentLight.diffuse.red * currentMaterial.diffuse.red;
 				output.green += specularFactor * diffuseFactor * currentLight.diffuse.green * currentMaterial.diffuse.green;
 				output.blue += specularFactor * diffuseFactor * currentLight.diffuse.blue * currentMaterial.diffuse.blue;
 			}
 		}
 		//Iterate over the reflection
-		contrast *= currentMaterial.reflectivity;
+		scene->contrast *= currentMaterial.reflectivity;
 		
 		//Calculate reflected ray start and direction
 		double reflect = 2.0 * scalarProduct(&incidentRay->direction, &surfaceNormal);
@@ -651,7 +650,7 @@ struct color rayTrace(struct lightRay *incidentRay, struct world *scene) {
 		
 		bounces++;
 		
-	} while ((contrast > 0.0) && (bounces <= scene->camera->bounces));
+	} while ((scene->contrast > 0.0) && (bounces <= scene->bounces));
 	
 	free(isectInfo);
 	free(shadowInfo);

src/camera.h
@@ -19,13 +19,6 @@ struct camera {
 	struct vector up;
 	struct vector left;
 	
-	int currentFrame;
-	
-	//FIXME: These 3 belong in renderer struct in renderer.h
-	double contrast;
-	bool areaLights;
-	int bounces;
-	
 	struct matrixTransform *transforms;
 	int transformCount;
 };

src/main.c
@@ -17,27 +17,12 @@
 #include "ui.h"
 
 int getFileSize(char *fileName);
+void initRenderer(struct renderer *renderer);
 int getSysCores();
 void freeMem();
 
 extern struct renderer mainRenderer;
 extern struct poly *polygonArray;
-
-void initRenderer(struct renderer *renderer) {
-	renderer->renderBuffer = NULL;
-	renderer->renderTiles = NULL;
-	renderer->tileCount = 0;
-	renderer->renderedTileCount = 0;
-	renderer->activeThreads = 0;
-	renderer->threadCount = getSysCores();
-	renderer->mode = saveModeNormal;
-	renderer->isRendering = false;
-	renderer->renderPaused = false;
-	renderer->avgTileTime = (time_t)1;
-	renderer->timeSampleCount = 1;
-	
-	renderer->scene = (struct world*)calloc(1, sizeof(struct world));
-}
 
 /**
  Main entry point
@@ -123,9 +108,9 @@ int main(int argc, char *argv[]) {
 	
 	//Verify sample count
 	if (mainRenderer.sampleCount < 1) logHandler(renderErrorInvalidSampleCount);
-	if (!mainRenderer.scene->camera->areaLights) mainRenderer.sampleCount = 1;
+	if (!mainRenderer.scene->areaLights) mainRenderer.sampleCount = 1;
 	
-	printf("\nStarting C-ray renderer for frame %i\n\n", mainRenderer.scene->camera->currentFrame);
+	printf("\nStarting C-ray renderer for frame %i\n\n", mainRenderer.currentFrame);
 	
 	//Print a useful warning to user if the defined tile size results in less renderThreads
 	if (mainRenderer.tileCount < mainRenderer.threadCount) {
@@ -144,7 +129,7 @@ int main(int argc, char *argv[]) {
 		printf("\n");
 	}
 	
-	printf("Using %i light bounces\n", mainRenderer.scene->camera->bounces);
+	printf("Using %i light bounces\n", mainRenderer.scene->bounces);
 	printf("Raytracing...\n");
 	
 	//Allocate memory and create array of pixels for image data
@@ -241,7 +226,7 @@ int main(int argc, char *argv[]) {
 	//Write to file
 	writeImage(mainRenderer.image);
 	
-	mainRenderer.scene->camera->currentFrame++;
+	mainRenderer.currentFrame++;
 	
 	freeMem();
 	
@@ -284,6 +269,33 @@ void freeMem() {
 		free(polygonArray);
 }
 
+void initRenderer(struct renderer *renderer) {
+	renderer->scene = (struct world*)calloc(1, sizeof(struct world));
+	renderer->image = (struct outputImage*)calloc(1, sizeof(struct outputImage));
+	renderer->renderTiles = NULL;
+	renderer->tileCount = 0;
+	renderer->renderedTileCount = 0;
+	renderer->renderBuffer = NULL;
+	renderer->uiBuffer = NULL;
+	renderer->activeThreads = 0;
+	renderer->isRendering = false;
+	renderer->renderPaused = false;
+	renderer->renderAborted = false;
+	renderer->avgTileTime = (time_t)1;
+	renderer->timeSampleCount = 1;
+	renderer->currentFrame = 0;
+	renderer->renderThreadInfo = (struct threadInfo*)calloc(1, sizeof(struct threadInfo));
+	renderer->mode = saveModeNormal;
+	renderer->tileOrder = renderOrderNormal;
+	renderer->inputFilePath = NULL;
+	renderer->threadCount = 0;
+	renderer->sampleCount = 0;
+	renderer->tileWidth = 0;
+	renderer->tileHeight = 0;
+	renderer->smoothShading = false;
+	renderer->antialiasing = false;
+	renderer->newRenderer = false;
+}
 
 /**
  Sleep for a given amount of milliseconds
@@ -302,7 +314,6 @@ void sleepMSec(int ms) {
 	usleep(ms * 1000);
 #endif
 }
-
 
 /**
  Get amount of logical processing cores on the system

src/renderer.c
@@ -363,7 +363,7 @@ DWORD WINAPI renderThread(LPVOID arg) {
 						
 						incidentRay.direction = direction;
 						incidentRay.rayType = rayTypeIncident;
-						incidentRay.remainingInteractions = mainRenderer.scene->camera->bounces;
+						incidentRay.remainingInteractions = mainRenderer.scene->bounces;
 						incidentRay.currentMedium.IOR = AIR_IOR;
 						
 						//For multi-sample rendering, we keep a running average of color values for each pixel

src/renderer.h
@@ -55,16 +55,13 @@ enum renderOrder {
  as well as information needed for the rendering routines.
  */
 struct renderer {
-	struct threadInfo *renderThreadInfo; //Info about threads
-#ifndef WINDOWS
-	pthread_attr_t renderThreadAttributes;
-#endif
+	//Source data
 	struct world *scene; //Scene to render
-	char *inputFilePath; //Directory to load input files from
+	
+	//State data
 	struct outputImage *image; //Output image
 	struct renderTile *renderTiles; //Array of renderTiles to render
 	int tileCount; //Total amount of render tiles
-	enum fileMode mode;
 	int renderedTileCount; //Completed render tiles
 	double *renderBuffer;  //Double-precision buffer for multisampling
 	unsigned char *uiBuffer; //UI element buffer
@@ -72,31 +69,31 @@ struct renderer {
 	bool isRendering;
 	bool renderPaused; //SDL listens for P key pressed, which sets this
 	bool renderAborted;//SDL listens for X key pressed, which sets this
-	bool smoothShading;//Unused
 	time_t avgTileTime;//Used for render duration estimation
 	int timeSampleCount;//Used for render duration estimation, amount of time samples captured
-	
-	//Prefs
-	int threadCount; //Amount of threads to render with
-	int sampleCount;
-	bool antialiasing;
-	bool newRenderer;
-	int tileWidth;
-	int tileHeight;
-	enum renderOrder tileOrder;
+	int currentFrame;
+	struct threadInfo *renderThreadInfo; //Info about threads
+#ifndef WINDOWS
+	pthread_attr_t renderThreadAttributes;
+#endif
 #ifdef UI_ENABLED
 	struct display *mainDisplay;
 #endif
+	
+	//Preferences data (Set by user)
+	enum fileMode mode;
+	enum renderOrder tileOrder;
+	char *inputFilePath; //Directory to load input files from
+	
+	int threadCount; //Amount of threads to render with
+	int sampleCount;
+	int tileWidth;
+	int tileHeight;
+	
+	bool smoothShading;//Unused
+	bool antialiasing;
+	bool newRenderer;
 };
-
-/*
- Move to renderer:
- 
- Move to UI:
- isFullScreen
- isBorderless
- windowScale
- */
 
 //Renderer
 #ifdef WINDOWS

src/scene.c
@@ -450,9 +450,6 @@ int parseCamera(struct renderer *r, const cJSON *data) {
 	
 	const cJSON *FOV = NULL;
 	const cJSON *aperture = NULL;
-	const cJSON *contrast = NULL;
-	const cJSON *bounces = NULL;
-	const cJSON *areaLights = NULL;
 	const cJSON *transforms = NULL;
 	
 	FOV = cJSON_GetObjectItem(data, "FOV");
@@ -471,29 +468,6 @@ int parseCamera(struct renderer *r, const cJSON *data) {
 		} else {
 			r->scene->camera->aperture = 0.0;
 		}
-	}
-	
-	contrast = cJSON_GetObjectItem(data, "contrast");
-	if (cJSON_IsNumber(contrast)) {
-		if (contrast->valuedouble >= 0.0) {
-			r->scene->camera->contrast = contrast->valuedouble;
-		} else {
-			r->scene->camera->contrast = 0.5;
-		}
-	}
-	
-	bounces = cJSON_GetObjectItem(data, "bounces");
-	if (cJSON_IsNumber(bounces)) {
-		if (bounces->valueint >= 1) {
-			r->scene->camera->bounces = bounces->valueint;
-		} else {
-			r->scene->camera->bounces = 1;
-		}
-	}
-	
-	areaLights = cJSON_GetObjectItem(data, "areaLights");
-	if (cJSON_IsBool(areaLights)) {
-		r->scene->camera->areaLights = cJSON_IsTrue(areaLights);
 	}
 	
 	transforms = cJSON_GetObjectItem(data, "transforms");
@@ -598,6 +572,9 @@ int parseScene(struct renderer *r, const cJSON *data) {
 	const cJSON *height = NULL;
 	const cJSON *fileType = NULL;
 	const cJSON *ambientColor = NULL;
+	const cJSON *contrast = NULL;
+	const cJSON *bounces = NULL;
+	const cJSON *areaLights = NULL;
 	const cJSON *lights = NULL;
 	const cJSON *spheres = NULL;
 	const cJSON *OBJs = NULL;
@@ -660,6 +637,29 @@ int parseScene(struct renderer *r, const cJSON *data) {
 		if (parseAmbientColor(r, ambientColor) == -1) {
 			return -1;
 		}
+	}
+	
+	contrast = cJSON_GetObjectItem(data, "contrast");
+	if (cJSON_IsNumber(contrast)) {
+		if (contrast->valuedouble >= 0.0) {
+			r->scene->contrast = contrast->valuedouble;
+		} else {
+			r->scene->contrast = 0.5;
+		}
+	}
+	
+	bounces = cJSON_GetObjectItem(data, "bounces");
+	if (cJSON_IsNumber(bounces)) {
+		if (bounces->valueint >= 0) {
+			r->scene->bounces = bounces->valueint;
+		} else {
+			r->scene->bounces = 0;
+		}
+	}
+	
+	areaLights = cJSON_GetObjectItem(data, "areaLights");
+	if (cJSON_IsBool(areaLights)) {
+		r->scene->areaLights = cJSON_IsTrue(areaLights);
 	}
 	
 	/*lights = cJSON_GetObjectItem(data, "lights");
@@ -795,11 +795,11 @@ int testBuild(struct renderer *r, char *inputFileName) {
 	r->mainDisplay->isBorderless = false;
 	r->mainDisplay->windowScale = 1.0;
 	
-	r->scene->camera->         FOV = 80.0;
-	r->scene->camera->    aperture = 0.0;
-	r->scene->camera->    contrast = 0.5;
-	r->scene->camera->bounces = 3;
-	r->scene->camera->areaLights = true;
+	r->scene->camera->     FOV = 80.0;
+	r->scene->camera->aperture = 0.0;
+	r->scene->        contrast = 0.5;
+	r->scene->         bounces = 3;
+	r->scene->      areaLights = true;
 	
 	//comment this block, and uncomment the next block below to toggle the detailed view of the lighting bug
 	addCamTransform(r->scene->camera, newTransformTranslate(970, 480, 600)); //Set pos here

src/scene.h
@@ -21,7 +21,6 @@ struct display;
 
 //World
 struct world {
-	
 	//Ambient background color.
 	struct color *ambientColor;
 	
@@ -45,6 +44,10 @@ struct world {
 	struct camera *camera;
 	int cameraCount;
 	
+	//Scene preferences
+	double contrast;
+	bool areaLights;
+	int bounces;
 };
 
 int testBuild(struct renderer *renderer, char *inputFileName);

commit 165661dbb14d1788445ad727d62ad63df7756c45

Undo a tweak to rayTrace() that broke it in 7a9758c2f4a0df2f8bc734aaeb9ec0f9d4c25160 (resolves #37)

src/raytrace.c
@@ -500,6 +500,7 @@ struct color rayTrace(struct lightRay *incidentRay, struct world *scene) {
 	//Raytrace a given light ray with a given scene, then return the color value for that ray
 	struct color output = {0.0,0.0,0.0,0.0};
 	int bounces = 0;
+	double contrast = scene->contrast;
 	
 	struct intersection *isectInfo = (struct intersection*)calloc(1, sizeof(struct intersection));
 	struct intersection *shadowInfo = (struct intersection*)calloc(1, sizeof(struct intersection));
@@ -572,7 +573,7 @@ struct color rayTrace(struct lightRay *incidentRay, struct world *scene) {
 			surfaceNormal = vectorScale(temp, &surfaceNormal);
 		} else {
 			//Ray didn't hit any object, set color to ambient
-			struct color temp = colorCoef(scene->contrast, scene->ambientColor);
+			struct color temp = colorCoef(contrast, scene->ambientColor);
 			output = addColors(&output, &temp);
 			break;
 		}
@@ -633,14 +634,14 @@ struct color rayTrace(struct lightRay *incidentRay, struct world *scene) {
 				double specularFactor = 1.0;//scalarProduct(&cameraRay.direction, &surfaceNormal) * contrast;
 				
 				//Calculate Lambert diffusion
-				double diffuseFactor = scalarProduct(&bouncedRay.direction, &surfaceNormal) * scene->contrast;
+				double diffuseFactor = scalarProduct(&bouncedRay.direction, &surfaceNormal) * contrast;
 				output.red += specularFactor * diffuseFactor * currentLight.diffuse.red * currentMaterial.diffuse.red;
 				output.green += specularFactor * diffuseFactor * currentLight.diffuse.green * currentMaterial.diffuse.green;
 				output.blue += specularFactor * diffuseFactor * currentLight.diffuse.blue * currentMaterial.diffuse.blue;
 			}
 		}
 		//Iterate over the reflection
-		scene->contrast *= currentMaterial.reflectivity;
+		contrast *= currentMaterial.reflectivity;
 		
 		//Calculate reflected ray start and direction
 		double reflect = 2.0 * scalarProduct(&incidentRay->direction, &surfaceNormal);
@@ -650,7 +651,7 @@ struct color rayTrace(struct lightRay *incidentRay, struct world *scene) {
 		
 		bounces++;
 		
-	} while ((scene->contrast > 0.0) && (bounces <= scene->bounces));
+	} while ((contrast > 0.0) && (bounces <= scene->bounces));
 	
 	free(isectInfo);
 	free(shadowInfo);