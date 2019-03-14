e1faa427626b5672bf61ad127256a14d7e5c157e
Valtteri Koskivuori <vkoskiv@gmail.com>
2019-03-14 13:21:59 +0200

    Remove string_extra and add it to the obj parser

--- src/libraries/obj_parser.c
+++ src/libraries/obj_parser.c
@@ -3,9 +3,22 @@
 #include <stdlib.h>
 #include "obj_parser.h"
 #include "list.h"
-#include "string_extra.h"
 
 #define WHITESPACE " \t\n\r"
+
+char strequal(const char *s1, const char *s2) {
+	if(strcmp(s1, s2) == 0) {
+		return 1;
+	}
+	return 0;
+}
+
+char contains(const char *haystack, const char *needle) {
+	if(strstr(haystack, needle) == NULL) {
+		return 0;
+	}
+	return 1;
+}
 
 void obj_free_half_list(list *listo)
 {
--- src/libraries/string_extra.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-char strequal(const char *s1, const char *s2);
-char contains(const char *haystack, const char *needle);
