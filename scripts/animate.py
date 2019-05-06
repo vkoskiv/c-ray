9a848d932031dba15f4fa6c124cb53ec735a4402
Valtteri Koskivuori <vkoskiv@gmail.com>
2019-05-06 03:01:18 +0300

    Make default animation 60fps

--- scripts/animate.py
+++ scripts/animate.py
@@ -10,12 +10,12 @@ with open(filename) as f:
     data = json.load(f)
 
 data["scene"]["outputFilePath"] = "output/20sample/"
-data["renderer"]["sampleCount"] = 20
+data["renderer"]["sampleCount"] = 100
 data["camera"]["transforms"][0]["x"] = 825
 
-for i in range(50):
+for i in range(120):
     data["scene"]["count"] = i
-    data["camera"]["transforms"][0]["x"] = data["camera"]["transforms"][0]["x"] + 5
+    data["camera"]["transforms"][0]["x"] = data["camera"]["transforms"][0]["x"] + 2.5
 
     proc = Popen('./bin/c-ray ', stdin=PIPE, shell=True)
     proc.stdin.write(json.dumps(data).encode())
