99af26a206cc73cd0b9169074bb51be569db05b2
Valtteri Koskivuori <vkoskiv@gmail.com>
2019-05-08 12:40:24 +0300

    Actually make the default animation 60FPS now. Also play with some math functions.

--- scripts/animate.py
+++ scripts/animate.py
@@ -3,6 +3,7 @@
 import json
 import sys
 from subprocess import *
+from math import *
 
 filename = 'input/scene.json'
 
@@ -10,12 +11,14 @@ with open(filename) as f:
     data = json.load(f)
 
 data["scene"]["outputFilePath"] = "output/20sample/"
-data["renderer"]["sampleCount"] = 100
-data["camera"]["transforms"][0]["x"] = 825
+data["renderer"]["sampleCount"] = 10
+data["renderer"]["bounces"] = 12
+data["camera"]["transforms"][0]["x"] = 675
 
-for i in range(120):
+for i in range(240):
     data["scene"]["count"] = i
-    data["camera"]["transforms"][0]["x"] = data["camera"]["transforms"][0]["x"] + 2.5
+    data["camera"]["transforms"][0]["x"] += 2.5
+    data["camera"]["transforms"][0]["y"] += 10*sin(data["camera"]["transforms"][0]["x"]/10)
 
     proc = Popen('./bin/c-ray ', stdin=PIPE, shell=True)
     proc.stdin.write(json.dumps(data).encode())
