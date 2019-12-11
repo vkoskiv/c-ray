47f8bcfb24e1e40334646a8dfe47a77368ca0d7c
Valtteri Koskivuori <vkoskiv@gmail.com>
2019-12-11 03:44:58 +0200

    Add assertion macros for future testing.

---
+++ src/utils/assert.h
@@ -0,0 +1,21 @@
+//
+//  assert.h
+//  C-ray
+//
+//  Created by Valtteri on 11.12.2019.
+//  Copyright © 2019 Valtteri Koskivuori. All rights reserved.
+//
+
+#pragma once
+
+void assertFailed(const char *file, const char *func, int line, const char *expr) {
+	printf("ASSERTION FAILED: In %s in function %s on line %i, expression \"%s\"", file, func, line, expr);
+	abort();
+}
+
+#define ASSERT(expr) \
+	if (!(expr)) \
+		assertFailed(__FILE__, __FUNCTION__, __LINE__, #expr)
+
+#define ASSERT_NOT_REACHED() \
+	assertFailed(__FILE__, __FUNCTION__, __LINE__, "ASSERT_NOT_REACHED")
