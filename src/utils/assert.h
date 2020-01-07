e945b0b0fdbe431a9a4432538864d403d8020623
Valtteri Koskivuori <vkoskiv@gmail.com>
2020-01-07 14:11:17 +0200

    Improve assert() macro

--- src/utils/assert.h
+++ src/utils/assert.h
@@ -8,13 +8,15 @@
 
 #pragma once
 
-void assertFailed(const char *file, const char *func, int line, const char *expr) {
+static void assertFailed(const char *file, const char *func, int line, const char *expr) {
 	printf("ASSERTION FAILED: In %s in function %s on line %i, expression \"%s\"", file, func, line, expr);
 	abort();
 }
 
 #define ASSERT(expr) \
-	if (!(expr)) \
+	if ((expr)) \
+		{} \
+	else \
 		assertFailed(__FILE__, __FUNCTION__, __LINE__, #expr)
 
 #define ASSERT_NOT_REACHED() \
