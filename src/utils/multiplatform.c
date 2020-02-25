e43b8a8c0c894ca896e1f658fe0d45b27cad7c67
Valtteri Koskivuori <vkoskiv@gmail.com>
2020-02-25 17:23:38 +0200

    Tidy up the macro formatting in multiplatform.c

--- src/utils/multiplatform.c
+++ src/utils/multiplatform.c
@@ -89,28 +89,28 @@ int getSysCores() {
 
 struct crMutex *createMutex() {
 	struct crMutex *new = calloc(1, sizeof(struct crMutex));
-	#ifdef WINDOWS
-		new->tileMutex = CreateMutex(NULL, FALSE, NULL);
-	#else
-		new->tileMutex = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
-	#endif
+#ifdef WINDOWS
+	new->tileMutex = CreateMutex(NULL, FALSE, NULL);
+#else
+	new->tileMutex = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
+#endif
 	return new;
 }
 
 void lockMutex(struct crMutex *m) {
-	#ifdef WINDOWS
-		WaitForSingleObject(m->tileMutex, INFINITE);
-	#else
-		pthread_mutex_lock(&m->tileMutex);
-	#endif
+#ifdef WINDOWS
+	WaitForSingleObject(m->tileMutex, INFINITE);
+#else
+	pthread_mutex_lock(&m->tileMutex);
+#endif
 }
 
 void releaseMutex(struct crMutex *m) {
-	#ifdef WINDOWS
-		ReleaseMutex(m->tileMutex);
-	#else
-		pthread_mutex_unlock(&m->tileMutex);
-	#endif
+#ifdef WINDOWS
+	ReleaseMutex(m->tileMutex);
+#else
+	pthread_mutex_unlock(&m->tileMutex);
+#endif
 }
 
 // Multiplatform threads
