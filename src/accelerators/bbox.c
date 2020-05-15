vkoskiv/c-ray

commit d1fcd05c5fa09d9fd13b6e517bea043ba5588fcd

Clean up kd-tree traversal implementation. It's now 1.17% faster as well on average.

src/accelerators/kdtree.c
@@ -176,32 +176,22 @@ int countNodes(const struct kdTreeNode *node) {
 
 bool rayIntersectsWithNode(const struct kdTreeNode *node, const struct lightRay *ray, struct hitRecord *isect) {
 	if (!node) return false;
-	//A bit of a hack, but it does work...!
 	float fakeIsect = 20000.0f;
-	if (rayIntersectWithAABB(node->bbox, ray, &fakeIsect)) {
-		bool hasHit = false;
-		
-		if (node->left != NULL || node->right != NULL) {
-			//Recurse down both sides
-			bool hitLeft  = rayIntersectsWithNode(node->left, ray, isect);
-			bool hitRight = rayIntersectsWithNode(node->right, ray, isect);
-			
-			return hitLeft || hitRight;
-		} else {
-			//This is a leaf, so check all polys
-			for (int i = 0; i < node->polyCount; ++i) {
-				struct poly p = polygonArray[node->polygons[i]];
-				if (rayIntersectsWithPolygon(ray, &p, &isect->distance, &isect->surfaceNormal, &isect->uv)) {
-					hasHit = true;
-					isect->type = hitTypePolygon;
-					isect->polyIndex = p.polyIndex;
-				}
-			}
-			if (hasHit) {
+	if (!rayIntersectWithAABB(node->bbox, ray, &fakeIsect)) return false;
+	if (node->left != NULL || node->right != NULL) {
+		//Recurse down both sides
+		bool hitLeft  = rayIntersectsWithNode(node->left, ray, isect);
+		bool hitRight = rayIntersectsWithNode(node->right, ray, isect);
+		return hitLeft || hitRight;
+	} else {
+		//This is a leaf, so check all polys
+		for (int i = 0; i < node->polyCount; ++i) {
+			struct poly p = polygonArray[node->polygons[i]];
+			if (rayIntersectsWithPolygon(ray, &p, &isect->distance, &isect->surfaceNormal, &isect->uv)) {
 				isect->didIntersect = true;
+				isect->type = hitTypePolygon;
+				isect->polyIndex = p.polyIndex;
 				return true;
-			} else {
-				return false;
 			}
 		}
 	}

commit 205625f3294145ae91324f6dfbe5cc0f6aa2a92a

Fix regression introduced in d1fcd05c5fa09d9fd13b6e517bea043ba5588fcd Tweak rayIntersectsWithAABB

src/accelerators/bbox.c
@@ -71,16 +71,7 @@ struct boundingBox *computeBoundingBox(const int *polys, const int count) {
 	return bbox;
 }
 
-
-/**
- Check if a ray intersects with an axis-aligned bounding box
-
- @param box Given bounding box to check against
- @param ray Given light ray to intersect
- @param t Current max t value for the ray
- @return true if intersected, false otherwise
- */
-bool rayIntersectWithAABB(const struct boundingBox *box, const struct lightRay *ray, float *t) {
+bool rayIntersectsWithAABB(const struct boundingBox *box, const struct lightRay *ray) {
 	//If a mesh has no polygons, it won't have a root bbox either.
 	if (!box) return false;
 	
@@ -98,16 +89,11 @@ bool rayIntersectWithAABB(const struct boundingBox *box, const struct lightRay *
 	
 	// if tmax < 0, ray is intersecting AABB, but the whole AABB is behind us
 	if (tmax < 0) {
-		*t = tmax;
 		return false;
 	}
-	
 	// if tmin > tmax, ray doesn't intersect AABB
 	if (tmin > tmax) {
-		*t = tmax;
 		return false;
 	}
-	
-	*t = tmin;
 	return true;
 }

src/accelerators/bbox.h
@@ -36,5 +36,4 @@ enum bboxAxis getLongestAxis(const struct boundingBox *bbox);
 /// Checks  for an intersection between a given ray and bounding box
 /// @param box Bounding box to check intersection against
 /// @param ray Ray to intersect
-/// @param t Distance the intersection occurred at along the ray
-bool rayIntersectWithAABB(const struct boundingBox *box, const struct lightRay *ray, float *t);
+bool rayIntersectsWithAABB(const struct boundingBox *box, const struct lightRay *ray);

src/accelerators/kdtree.c
@@ -176,23 +176,26 @@ int countNodes(const struct kdTreeNode *node) {
 
 bool rayIntersectsWithNode(const struct kdTreeNode *node, const struct lightRay *ray, struct hitRecord *isect) {
 	if (!node) return false;
-	float fakeIsect = 20000.0f;
-	if (!rayIntersectWithAABB(node->bbox, ray, &fakeIsect)) return false;
-	if (node->left != NULL || node->right != NULL) {
+	if (!rayIntersectsWithAABB(node->bbox, ray)) return false;
+	if (node->left || node->right) {
 		//Recurse down both sides
 		bool hitLeft  = rayIntersectsWithNode(node->left, ray, isect);
 		bool hitRight = rayIntersectsWithNode(node->right, ray, isect);
 		return hitLeft || hitRight;
 	} else {
+		bool hasHit = false;
 		//This is a leaf, so check all polys
 		for (int i = 0; i < node->polyCount; ++i) {
 			struct poly p = polygonArray[node->polygons[i]];
 			if (rayIntersectsWithPolygon(ray, &p, &isect->distance, &isect->surfaceNormal, &isect->uv)) {
-				isect->didIntersect = true;
+				hasHit = true;
 				isect->type = hitTypePolygon;
 				isect->polyIndex = p.polyIndex;
-				return true;
 			}
+		}
+		if (hasHit) { // Return only after checking every polygon to make sure we got the closest one.
+			isect->didIntersect = true;
+			return true;
 		}
 	}
 	return false;