5a5de1bdfd6a80c0e37a2fdb67384fc450817cae
Valtteri Koskivuori <vkoskiv@gmail.com>
2020-07-28 05:20:21 +0300

    And finally add the original logo.h
    Hopefully by splitting this into three commits, we didn't duplicate the data in the git history.

---
+++ src/utils/logo.h
@@ -0,0 +1,17 @@
+//
+//  logo.h
+//  C-ray
+//
+//  Created by Valtteri on 2.6.2020.
+//  Copyright © 2020 Valtteri Koskivuori. All rights reserved.
+//
+
+// The C-ray logo, encoded as a PNG.
+// This will be decoded and passed to SDL2 in initDisplay
+// This source file is 227kB, but the final PNG embedded in the
+// binary is only 37kB
+// Decoding and setting this logo adds about ~130ms to the initial setup run-time.
+unsigned int logo_png_data_len = 37566;
+unsigned char logo_png_data[] = {
+#include "logo_bytes.txt"
+};
