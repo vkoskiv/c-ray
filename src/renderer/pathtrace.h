4bfad1d396f2e810b7b6fe73fb63ac3ffd80b4fb
Valtteri Koskivuori <vkoskiv@gmail.com>
2020-09-02 00:05:11 +0300

    Slightly fix status printing for the interactive mode. Still not fully there yet, the rendertime estimation is way off!

--- src/renderer/pathtrace.h
+++ src/renderer/pathtrace.h
@@ -24,13 +24,13 @@ struct world;
  */
 struct hitRecord {
 	struct lightRay incident;		//Light ray that encountered this intersection
-	struct material material;	    //Material of the intersected object
+	struct material material;		//Material of the intersected object
 	struct vector hitPoint;			//Hit point vector in 3D space
 	struct vector surfaceNormal;	//Surface normal at that point of intersection
 	struct coord uv;				//UV barycentric coordinates for intersection point
 	float distance;					//Distance to intersection point
 	struct poly *polygon;			//ptr to polygon that was encountered
-	int instIndex;                  //Instance index, negative if no intersection
+	int instIndex;					//Instance index, negative if no intersection
 };
 
 
