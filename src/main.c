fdb8abb26521546360a4a08cec36dfc45d60c68a
Valtteri Koskivuori <vkoskiv@gmail.com>
2020-11-05 00:05:19 +0200

    Remove unneeded main.h header

--- src/main.c
+++ src/main.c
@@ -8,7 +8,6 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
-#include "main.h"
 
 #include "c-ray.h"
 
--- src/main.h
+++
@@ -1,9 +0,0 @@
-//
-//  main.h
-//  C-ray
-//
-//  Created by Valtteri Koskivuori on 12/02/2015.
-//  Copyright © 2015-2020 Valtteri Koskivuori. All rights reserved.
-//
-
-#pragma once
