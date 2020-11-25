467ee4f407bb9ffb8a83c31d7357cb130f6bfcc2
Valtteri Koskivuori <vkoskiv@gmail.com>
2020-11-25 23:13:59 +0200

    Remove previous mtl/wavefront loader attempts

--- src/utils/loaders/formats/wavefront/wavefront.c
+++ src/utils/loaders/formats/wavefront/wavefront.c
@@ -13,76 +13,14 @@
 #include "../../../../datatypes/vector.h"
 #include "../../../../datatypes/poly.h"
 #include "../../../../datatypes/material.h"
+#include "../../../../datatypes/vertexbuffer.h"
+#include "../../../../datatypes/vertexbuffer.h"
 #include "../../../logging.h"
 #include "../../../string.h"
-#include "../../../../datatypes/vertexbuffer.h"
 #include "../../../fileio.h"
 #include "../../../assert.h"
 #include "../../../textbuffer.h"
-#include "../../../../datatypes/vertexbuffer.h"
-
 #include "mtlloader.h"
-
-#define ws " \t\n\r"
-
-static int parseIndices(int *vertexIndex, int *normalIndex, int *textureIndex) {
-	
-	char *tempstr;
-	char *token;
-	int vertexCount = 0;
-	
-	while ((token = strtok(NULL, ws)) != NULL) {
-		
-		//texture and normal indices are optional
-		if (textureIndex != NULL) {
-			textureIndex[vertexCount] = 0;
-		}
-		if (normalIndex != NULL) {
-			normalIndex[vertexCount] = 0;
-		}
-		
-		vertexIndex[vertexCount] = atoi(token);
-		
-		if (stringContains(token, "//")) {
-			//Only normal and vertex
-			tempstr = strchr(token, '/');
-			tempstr++;
-			normalIndex[vertexCount] = atoi(++tempstr);
-		} else if (stringContains(token, "/")) {
-			tempstr = strchr(token, '/');
-			textureIndex[vertexCount] = atoi(++tempstr);
-			if (stringContains(tempstr, "/")) {
-				tempstr = strchr(tempstr, '/');
-				normalIndex[vertexCount] = atoi(++tempstr);
-			}
-		}
-		
-		vertexCount++;
-	}
-	
-	return vertexCount;
-}
-
-static struct poly parsePolyOld(struct mesh mesh) {
-	struct poly p;
-	p.vertexCount = parseIndices(p.vertexIndex, p.normalIndex, p.textureIndex);
-	
-	/*convertIndices(vertexCount, p.vertexIndex);
-	convertIndices(normalCount, p.normalIndex);
-	convertIndices(textureCount, p.textureIndex);*/
-	
-	for (int i = 0; i < p.vertexCount; ++i) {
-		if (p.normalIndex[i] == 0) {
-			p.hasNormals = false;
-		}
-		
-		p.vertexIndex[i] = mesh.firstVectorIndex + (p.vertexIndex[i] - 1);
-		p.normalIndex[i] = mesh.firstNormalIndex + (p.normalIndex[i] - 1);
-		p.textureIndex[i] = mesh.firstTextureIndex + (p.textureIndex[i] - 1);
-	}
-	
-	return p;
-}
 
 static int findMaterialIndex(struct mesh *mesh, char *mtlName) {
 	for (int i = 0; i < mesh->materialCount; ++i) {
@@ -91,11 +29,6 @@ static int findMaterialIndex(struct mesh *mesh, char *mtlName) {
 		}
 	}
 	return 0;
-}
-
-static struct mesh parseOBJMesh(textBuffer *segment) {
-	(void)segment;
-	return (struct mesh){0};
 }
 
 // Count lines starting with thing
@@ -110,11 +43,6 @@ static size_t count(textBuffer *buffer, const char *thing) {
 	head = firstLine(buffer);
 	return thingCount;
 }
-
-/*enum currentMode {
-	None,
-	Mesh
-};*/
 
 static struct vector parseVertex(lineBuffer *line) {
 	ASSERT(line->amountOf.tokens == 4);
@@ -198,7 +126,7 @@ struct mesh *parseWavefront(const char *filePath, size_t *finalMeshCount) {
 	
 	struct material *materialSet = NULL;
 	int materialCount = 0;
-	int currentMaterial = 0;
+	int currentMaterialIndex = 0;
 	
 	struct mesh *meshes = calloc(meshCount, sizeof(*meshes));
 	struct mesh *currentMeshPtr = NULL;
@@ -230,8 +158,8 @@ struct mesh *parseWavefront(const char *filePath, size_t *finalMeshCount) {
 			currentMeshPtr->normalCount++;
 		} else if (stringEquals(first, "f")) {
 			struct poly p = parsePolygon(line);
-			p.materialIndex = currentMaterial;
 			fixIndices(&p, fileVertices, fileTexCoords, fileNormals);
+			p.materialIndex = currentMaterialIndex;
 			p.hasNormals = p.normalIndex[0] != -1;
 			polygons[currentPoly++] = p;
 		} else if (stringEquals(first, "mtllib")) {
@@ -296,185 +224,4 @@ struct mesh *parseWavefront(const char *filePath, size_t *finalMeshCount) {
 	
 	//exit(0);
 	return meshes;
-}
-
-struct mesh *parseOBJFileNah(char *filePath, size_t *meshCountOut) {
-	logr(debug, "Loading OBJ file\n");
-	char *rawText = loadFile(filePath, NULL);
-	textBuffer *file = newTextBuffer(rawText);
-	
-	//Figure out how many meshes this file contains
-	size_t meshCount = count(file, "o");
-	if (!meshCount) return NULL;
-
-	//Get the offsets
-	char *head = firstLine(file);
-	lineBuffer line = {0};
-	size_t *meshOffsets = malloc(meshCount * sizeof(*meshOffsets));
-	int i = 0;
-	while (head) {
-		fillLineBuffer(&line, head, " ");
-		char *first = firstToken(&line);
-		if (first[0] == 'o') meshOffsets[i++] = file->current.line;
-		head = nextLine(file);
-	}
-	logr(debug, "They start on lines:\n");
-	
-	for (size_t l = 0; l < meshCount; ++l) {
-		logr(debug, "%zu\n", meshOffsets[l]);
-	}
-	head = firstLine(file);
-	
-	struct mesh *meshes = calloc(meshCount, sizeof(*meshes));
-	
-	for (size_t m = 0; m < meshCount; ++m) {
-		size_t start = meshOffsets[m];
-		size_t end = meshCount == 1 ? file->amountOf.lines : meshOffsets[m + 1] - meshOffsets[m];
-		textBuffer *segment = newTextView(file, start, end);
-		meshes[m] = parseOBJMesh(segment);
-		freeTextBuffer(segment);
-	}
-	
-	free(meshOffsets);
-	destroyLineBuffer(&line);
-	freeTextBuffer(file);
-	if (meshCountOut) *meshCountOut = meshCount;
-	exit(0);
-	return NULL;
-}
-
-//Parse a Wavefront OBJ file and return a generic mesh.
-//Note: This will also alter the global vertex arrays
-//We will add vertices to them as the OBJ is loaded.
-struct mesh *parseOBJFilea(char *filePath) {
-	struct mesh *newMesh = calloc(1, sizeof(*newMesh));
-	
-	int linenum = 0;
-	char *token;
-	char currLine[500];
-	FILE *fileStream;
-	
-	fileStream = fopen(filePath, "r");
-	if (fileStream == 0) {
-		logr(warning, "OBJ not found at %s\n", filePath);
-		free(newMesh);
-		return NULL;
-	}
-	
-	//First, find vertex counts
-	int vCount = 0;
-	int nCount = 0;
-	int tCount = 0;
-	int pCount = 0;
-	
-	while (fgets(currLine, 500, fileStream)) {
-		token = strtok(currLine, ws);
-		//skip comments
-		if(token == NULL || stringEquals(token, "#")) {
-			continue;
-		} else if (stringEquals(token, "v")) {
-			vCount++;
-		} else if (stringEquals(token, "vn")) {
-			nCount++;
-		} else if (stringEquals(token, "vt")) {
-			tCount++;
-		} else if (stringEquals(token, "f")) {
-			pCount++;
-		}
-	}
-	fseek(fileStream, 0, SEEK_SET);
-	
-	newMesh->firstVectorIndex = vertexCount;
-	newMesh->firstNormalIndex = normalCount;
-	newMesh->firstTextureIndex = textureCount;
-	
-	newMesh->vertexCount = vCount;
-	newMesh->normalCount = nCount;
-	newMesh->textureCount = tCount;
-	newMesh->polyCount = pCount;
-	
-	vertexCount += vCount;
-	g_vertices = realloc(g_vertices, vertexCount * sizeof(struct vector));
-	normalCount += nCount;
-	g_normals = realloc(g_normals, normalCount * sizeof(struct vector));
-	textureCount += tCount;
-	g_textureCoords = realloc(g_textureCoords, textureCount * sizeof(struct coord));
-	
-	newMesh->polygons = malloc(pCount * sizeof(struct poly));
-	
-	//newMesh->name;
-	//newMesh->materials;
-	//newMesh->materialCount;
-	
-	int currMatIdx = 0;
-	int currVecIdx = 0;
-	int currNorIdx = 0;
-	int currTexIdx = 0;
-	int currPolIdx = 0;
-	
-	while (fgets(currLine, 500, fileStream)) {
-		token = strtok(currLine, ws);
-		linenum++;
-		
-		if (token == NULL || stringEquals(token, "#")) {
-			//skip comments
-			continue;
-		} else if (stringEquals(token, "v")) {
-			//Vertex
-			//g_vertices[newMesh->firstVectorIndex + currVecIdx] = parseVertex();
-			currVecIdx++;
-		} else if (stringEquals(token, "vn")) {
-			//Normal
-			//g_normals[newMesh->firstNormalIndex + currNorIdx] = parseVertex();
-			currNorIdx++;
-		} else if (stringEquals(token, "vt")) {
-			//Texture coord
-			//g_textureCoords[newMesh->firstTextureIndex + currTexIdx] = parseCoord();
-			currTexIdx++;
-		} else if (stringEquals(token, "f")) {
-			//Polygon
-			//struct poly p = parsePoly(*newMesh);
-			//p.materialIndex = currMatIdx;
-			//newMesh->polygons[currPolIdx] = p;
-			currPolIdx++;
-		} else if (stringEquals(token, "usemtl")) {
-			//current material index
-			currMatIdx = findMaterialIndex(newMesh, strtok(NULL, ws));
-		} else if (stringEquals(token, "mtllib")) {
-			//new material
-			int mtlCount = 0;
-			//I am so very sorry for this. I didn't mean to. Just wanted to get it working.
-			char *fullPath = (char*)calloc(1024, sizeof(char));
-			sprintf(fullPath, "%s", filePath);
-			char key = '\0';
-			int i = (int)strcspn(filePath, &key);
-			fullPath[i-3] = 'm';
-			fullPath[i-2] = 't';
-			fullPath[i-1] = 'l';
-			
-			struct material *newMats = parseMTLFile(fullPath, &mtlCount);
-			
-			if (newMats != NULL) {
-				newMesh->materialCount = mtlCount;
-				newMesh->materials = newMats;
-			} else {
-				newMesh->materials = calloc(1, sizeof(*newMesh->materials));
-				newMesh->materials[0] = warningMaterial();
-				newMesh->materialCount = 1;
-			}
-			free(fullPath);
-		} else if (stringEquals(token, "o")) {
-			//object name
-			newMesh->name = stringCopy(strtok(NULL, ws));
-		} else if (stringEquals(token, "s")) {
-			//smoothShading
-			//TODO
-		} else {
-			logr(warning, "Unrecognized command '%s' in OBJ file %s on line %i\n", token, filePath, linenum);
-		}
-	}
-	
-	fclose(fileStream);
-	
-	return newMesh;
 }
