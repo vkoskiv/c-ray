dd83ee80fc54913d057518761b394890da5f1931
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2020-12-04 01:29:03 +0100

    Fix the tests to reflect the new hash table API

--- tests/test_hashtable.h
+++ tests/test_hashtable.h
@@ -12,49 +12,40 @@
 bool hashtable_mixed(void) {
 	bool pass = true;
 	
-	struct hashtable *table = newTable();
+	struct constantsDatabase *database = newConstantsDatabase();
 	
-	setVector(table, "key0", (struct vector){1.0f, 2.0f, 3.0f});
-	setFloat(table, "key1", 123.4f);
-	setTag(table, "key2");
-	setString(table, "key3", "This is my cool string");
-	setInt(table, "key4", 1234);
+	setDatabaseVector(database, "key0", (struct vector){1.0f, 2.0f, 3.0f});
+	setDatabaseFloat(database, "key1", 123.4f);
+	setDatabaseTag(database, "key2");
+	setDatabaseString(database, "key3", "This is my cool string");
+	setDatabaseInt(database, "key4", 1234);
 	
-	test_assert(table);
-	test_assert(vecEquals(getVector(table, "key0"), (struct vector){1.0f, 2.0f, 3.0f}));
-	test_assert(getFloat(table, "key1") == 123.4f);
-	test_assert(exists(table, "key2"));
-	test_assert(stringEquals(getString(table, "key3"), "This is my cool string"));
-	test_assert(getInt(table, "key4") == 1234);
+	test_assert(database);
+	test_assert(vecEquals(getDatabaseVector(database, "key0"), (struct vector){1.0f, 2.0f, 3.0f}));
+	test_assert(getDatabaseFloat(database, "key1") == 123.4f);
+	test_assert(existsInDatabase(database, "key2"));
+	test_assert(stringEquals(getDatabaseString(database, "key3"), "This is my cool string"));
+	test_assert(getDatabaseInt(database, "key4") == 1234);
 	
-	freeTable(table);
+	freeConstantsDatabase(database);
 	
 	return pass;
-}
-
-size_t used_count(struct hashtable *t) {
-	size_t used = 0;
-	for (size_t i = 0; i < t->size; ++i) {
-		if (t->data[i].used) used++;
-	}
-	return used;
 }
 
 bool hashtable_fill(void) {
 	bool pass = true;
 	
-	struct hashtable *table = newTable();
+	struct constantsDatabase *database = newConstantsDatabase();
 	
 	char buf[20];
 	
-	// Collides at i == 203
-	for (size_t i = 0; i < 200 && pass; ++i) {
-		test_assert(used_count(table) == i);
+	for (size_t i = 0; i < 400 && pass; ++i) {
+		test_assert(database->hashtable.elemCount == i);
 		sprintf(buf, "key%lu", i);
-		setTag(table, buf);
+		setDatabaseTag(database, buf);
 	}
 	
-	freeTable(table);
+	freeConstantsDatabase(database);
 	
 	return pass;
 }
