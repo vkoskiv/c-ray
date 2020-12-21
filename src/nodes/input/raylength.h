c2fb8cb2bdc3e1b6476847107e3c0b7f652be0ec
Valtteri Koskivuori <vkoskiv@gmail.com>
2020-12-22 01:12:02 +0200

    Implement ray length node

---
+++ src/nodes/input/raylength.h
@@ -0,0 +1,11 @@
+//
+//  raylength.h
+//  C-Ray
+//
+//  Created by Valtteri Koskivuori on 21/12/2020.
+//  Copyright © 2020 Valtteri Koskivuori. All rights reserved.
+//
+
+#pragma once
+
+const struct valueNode *newRayLength(const struct world *world);
