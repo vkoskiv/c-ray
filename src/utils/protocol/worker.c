15c4d04e090a7041227c71d199655ccb4170a563
Valtteri Koskivuori <vkoskiv@gmail.com>
2021-04-19 11:27:09 +0300

    Set random sampling for workers as well

--- src/utils/protocol/worker.c
+++ src/utils/protocol/worker.c
@@ -159,7 +159,7 @@ static void *workerThread(void *arg) {
 				for (int x = tile.begin.x; x < tile.end.x; ++x) {
 					if (r->state.renderAborted) return 0;
 					uint32_t pixIdx = (uint32_t)(y * r->prefs.imageWidth + x);
-					initSampler(sampler, Halton, threadState->completedSamples - 1, r->prefs.sampleCount, pixIdx);
+					initSampler(sampler, Random, threadState->completedSamples - 1, r->prefs.sampleCount, pixIdx);
 					
 					struct color output = textureGetPixel(r->state.renderBuffer, x, y, false);
 					struct lightRay incidentRay = getCameraRay(r->scene->camera, x, y, sampler);
