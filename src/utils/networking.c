c98483069f040336aa72954e5abe31586447de9f
Valtteri Koskivuori <vkoskiv@gmail.com>
2021-08-22 22:15:37 +0300

    Fxi missing ntohll/htonll on PowerPC

--- src/utils/networking.c
+++ src/utils/networking.c
@@ -28,13 +28,19 @@
 #include "args.h"
 
 #ifndef __APPLE__
-#if __BIG_ENDIAN__
-#define htonll(x)   (x)
-#define ntohll(x)   (x)
+	#if __BIG_ENDIAN__
+		#define htonll(x)   (x)
+		#define ntohll(x)   (x)
+	#else
+		#define htonll(x)   ((((uint64_t)htonl(x & 0xFFFFFFFF)) << 32) + htonl(x >> 32))
+		#define ntohll(x)   ((((uint64_t)ntohl(x & 0xFFFFFFFF)) << 32) + ntohl(x >> 32))
+	#endif
 #else
-#define htonll(x)   ((((uint64_t)htonl(x & 0xFFFFFFFF)) << 32) + htonl(x >> 32))
-#define ntohll(x)   ((((uint64_t)ntohl(x & 0xFFFFFFFF)) << 32) + ntohl(x >> 32))
-#endif
+	// PowerPC Mac, probably.
+	#if __BIG_ENDIAN__
+		#define htonll(x)   (x)
+		#define ntohll(x)   (x)
+	#endif
 #endif
 
 #define C_RAY_HEADERSIZE 8
