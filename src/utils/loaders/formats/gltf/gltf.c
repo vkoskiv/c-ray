a87d8bb6e2249fdc534d46aa926737096731e258
Valtteri Koskivuori <vkoskiv@gmail.com>
2021-11-22 19:29:35 +0200

    More work on the glTF parser from a while ago
    I don't remember anything I did, but it looks like this implements
    buffer view, accessor and texture parsing.
    The glTF parser is far from done, but is progressing.

--- src/utils/loaders/formats/gltf/gltf.c
+++ src/utils/loaders/formats/gltf/gltf.c
@@ -16,26 +16,34 @@
 #include "../../../logging.h"
 #include <string.h>
 #include "../../../fileio.h"
+#include "../../../../datatypes/image/texture.h"
+#include "../../textureloader.h"
 
 enum accessor_type {
-	VEC3,
+	UNKNOWN,
 	VEC2,
+	VEC3,
 	SCALAR,
 };
 
-//TODO: Max and min, what are those used for even?
+//TODO: Max and min, what are those used for even? Integrity checking?
 struct accessor {
-	int buffer_view;
-	int componentType; // enum? Not sure what this is
-	size_t count;
+	size_t buffer_view_idx;
+	size_t byte_offset;
 	enum accessor_type type;
+	size_t count;
 };
 
 struct buffer_view {
-	int buffer;
+	size_t buffer_idx;
 	size_t byte_length;
 	size_t byte_offset;
+	size_t byte_stride;
 };
+
+size_t get_int_or_zero(const cJSON *object, const char *key) {
+	return cJSON_HasObjectItem(object, key) ? cJSON_GetObjectItem(object, key)->valueint : 0;
+}
 
 char *parse_buffer(const cJSON *data) {
 	const cJSON *byteLength = cJSON_GetObjectItem(data, "byteLength");
@@ -55,7 +63,7 @@ char *parse_buffer(const cJSON *data) {
 		size_t decoded_length = 0;
 		buffer = b64decode(base64data, encoded_length, &decoded_length);
 		if (decoded_length != expected_bytes) {
-			logr(warning, "Invalid buffer while parsing glTF. base64 decoded length of %lu, expected %lu", decoded_length, expected_bytes);
+			logr(warning, "Invalid buffer while parsing glTF. base64 decoded length of %lu, expected %lu\n", decoded_length, expected_bytes);
 			free(buffer);
 			return NULL;
 		}
@@ -99,41 +107,111 @@ struct buffer_view *parse_buffer_views(const cJSON *data, size_t *amount) {
 	buffer_view_amount = cJSON_GetArraySize(data);
 	struct buffer_view *views = calloc(buffer_view_amount, sizeof(*views));
 	for (size_t i = 0; i < buffer_view_amount; ++i) {
-		cJSON *element = cJSON_GetArrayItem(data, (int)i);
+		const cJSON *element = cJSON_GetArrayItem(data, (int)i);
 		// FIXME: Validate
-		views[i].buffer = cJSON_GetObjectItem(element, "buffer")->valueint;
-		views[i].byte_length = cJSON_GetObjectItem(element, "byteLength")->valueint;
-		views[i].byte_offset = cJSON_GetObjectItem(element, "byteOffset")->valueint;
+		views[i].buffer_idx = get_int_or_zero(element, "buffer");
+		views[i].byte_length = get_int_or_zero(element, "byteLength");
+		views[i].byte_offset = get_int_or_zero(element, "byteOffset");
+		views[i].byte_stride = get_int_or_zero(element, "byteStride");
 	}
 	
 	if (amount) *amount = buffer_view_amount;
 	return views;
 }
 
-struct accessor *parse_accessors(const cJSON *data, size_t *amount) {
-	// TODO
-	return NULL;
+enum accessor_type accessor_type_for_string(const char *str) {
+	if (stringEquals(str, "VEC2"))
+		return VEC2;
+	if (stringEquals(str, "VEC3"))
+		return VEC3;
+	if (stringEquals(str, "SCALAR"))
+		return SCALAR;
+	return UNKNOWN;
 }
 
-static int parse_glTF(struct renderer *r, char *input, bool only_assets) {
-	const cJSON *data = cJSON_Parse(input);
+struct accessor *parse_accessors(const cJSON *data, size_t *amount) {
+	size_t accessor_amount = 0;
+	if (!cJSON_IsArray(data)) return NULL;
+	
+	accessor_amount = cJSON_GetArraySize(data);
+	struct accessor *accessors = calloc(accessor_amount, sizeof(*accessors));
+	for (size_t i = 0; i < accessor_amount; ++i) {
+		// FIXME: Validate
+		const cJSON *element = cJSON_GetArrayItem(data, (int)i);
+		accessors[i].buffer_view_idx = cJSON_GetObjectItem(element, "bufferView")->valueint;
+		accessors[i].byte_offset = cJSON_HasObjectItem(element, "byteOffset") ? cJSON_GetObjectItem(element, "byteOffset")->valueint : 0;
+		accessors[i].type = accessor_type_for_string(cJSON_GetObjectItem(element, "type")->valuestring);
+		accessors[i].count = cJSON_GetObjectItem(element, "count")->valueint;
+	}
+	
+	if (amount) *amount = accessor_amount;
+	return accessors;
+}
+
+//TODO: Figure out what the stride means and how it works
+// the glTF doc was quite vague about it. For now, we ignore it.
+char *bytes_from_buffer_view(const struct buffer_view *view, char **buffers) {
+	char *data = malloc(view->byte_length);
+	memcpy(data, buffers[view->buffer_idx] + view->byte_offset, view->byte_length);
+	return data;
+}
+
+struct texture *parse_images(const cJSON *data, size_t *amount, const struct buffer_view *views, char **buffers) {
+	if (!cJSON_IsArray(data)) return NULL;
+	
+	size_t image_amount = 0;
+	image_amount = cJSON_GetArraySize(data);
+	struct texture *images = calloc(image_amount, sizeof(*images));
+	for (size_t i = 0; i < image_amount; ++i) {
+		const cJSON *element = cJSON_GetArrayItem(data, (int)i);
+		if (cJSON_HasObjectItem(element, "uri")) {
+			const cJSON *uri = cJSON_GetObjectItem(element, "uri");
+			if (!cJSON_IsString(uri)) break;
+			char *uri_string = uri->valuestring;
+			images[i] = *load_texture(uri_string, NULL);
+		} else {
+			const cJSON *buffer_view = cJSON_GetObjectItem(element, "bufferView");
+			const cJSON *mime_type = cJSON_GetObjectItem(element, "mimeType");
+			if (!cJSON_IsNumber(buffer_view) || !cJSON_IsString(mime_type)) break;
+			size_t buffer_view_idx = buffer_view->valueint;
+			char *bytes = bytes_from_buffer_view(&views[buffer_view_idx], buffers);
+			images[i] = *load_texture_from_buffer((unsigned char *)data, (unsigned int)views[buffer_view_idx].byte_length, NULL);
+			free(bytes);
+		}
+	}
+	if (amount) *amount = image_amount;
+	return images;
+}
+
+struct mesh *parse_glTF_meshes(const char *filePath, size_t *meshCount) {
+	size_t file_bytes = 0;
+	char *contents = loadFile(filePath, &file_bytes);
+	const cJSON *data = cJSON_Parse(contents);
+	
+	const cJSON *asset = cJSON_GetObjectItem(data, "asset");
+	if (asset) {
+		const cJSON *generator = cJSON_GetObjectItem(asset, "generator");
+		const cJSON *version = cJSON_GetObjectItem(asset, "version");
+		if (cJSON_IsString(generator) && cJSON_IsString(version)) {
+			logr(debug, "Parsing glTF file \"%s\" Generator: \"%s\", glTF version %s\n", filePath, generator->valuestring, version->valuestring);
+		}
+	}
 	
 	size_t buffers_count = 0;
-	char **buffers = parse_buffers(cJSON_GetObjectItem(data, "buffers"), &buffers_count);
-	
-	size_t buffer_views_count = 0;
-	struct buffer_view *views = parse_buffer_views(data, &buffer_views_count);
+	char **buffers = parse_buffers(data, &buffers_count);
 	
 	//TODO: Validate that buffer views are valid, perhaps?
+	size_t buffer_views_count = 0;
+	struct buffer_view *buffer_views = parse_buffer_views(cJSON_GetObjectItem(data, "bufferViews"), &buffer_views_count);
 	
+	size_t accessors_count = 0;
+	struct accessor *accessors = parse_accessors(cJSON_GetObjectItem(data, "accessors"), &accessors_count);
+	(void)accessors; //TODO
 	
+	size_t texture_count = 0;
+	struct texture *textures = parse_images(cJSON_GetObjectItem(data, "images"), &texture_count, buffer_views, buffers);
+	(void)textures; //TODO
 	
-	return 0;
-}
-
-struct mesh *parseglTF(const char *filePath, size_t *meshCount) {
-	//TODO
-	(void)filePath;
 	if (meshCount) *meshCount = 0;
 	return NULL;
 }
