2c8bf0d553f111ce83bdb3267d49e697660af889
Valtteri Koskivuori <vkoskiv@gmail.com>
2023-11-11 22:55:14 +0200

    Use dyn_array in file cache

--- src/utils/filecache.c
+++ src/utils/filecache.c
@@ -1,9 +1,9 @@
 //
 //  filecache.c
-//  C-Ray
+//  c-ray
 //
 //  Created by Valtteri Koskivuori on 29/03/2021.
-//  Copyright © 2021-2022 Valtteri Koskivuori. All rights reserved.
+//  Copyright © 2021-2023 Valtteri Koskivuori. All rights reserved.
 //
 
 #include "filecache.h"
@@ -15,8 +15,8 @@
 #include "logging.h"
 
 bool cache_contains(const struct file_cache *cache, const char *path) {
-	for (size_t i = 0; i < cache->file_count; ++i) {
-		if (stringEquals(path, cache->files[i].path)) {
+	for (size_t i = 0; i < cache->files.count; ++i) {
+		if (stringEquals(path, cache->files.items[i].path)) {
 			return true;
 		}
 	}
@@ -28,23 +28,22 @@ void cache_store(struct file_cache *cache, const char *path, const void *data, s
 		logr(debug, "File %s already cached, skipping.\n", path);
 		return;
 	}
-	cache->files = realloc(cache->files, ++cache->file_count * sizeof(*cache->files));
 	struct file file;
 	file.path = stringCopy(path);
 	file.data = malloc(length);
 	file.size = length;
 	memcpy(file.data, data, length);
-	cache->files[cache->file_count - 1] = file;
+	file_arr_add(&cache->files, file);
 	logr(debug, "Cached file %s\n", path);
 }
 
 void *cache_load(const struct file_cache *cache, const char *path, size_t *length) {
-	for (size_t i = 0; i < cache->file_count; ++i) {
-		if (stringEquals(path, cache->files[i].path)) {
-			if (length) *length = cache->files[i].size;
-			char *ret = malloc(cache->files[i].size + 1);
-			memcpy(ret, cache->files[i].data, cache->files[i].size);
-			ret[cache->files[i].size] = 0;
+	for (size_t i = 0; i < cache->files.count; ++i) {
+		if (stringEquals(path, cache->files.items[i].path)) {
+			if (length) *length = cache->files.items[i].size;
+			char *ret = malloc(cache->files.items[i].size + 1);
+			memcpy(ret, cache->files.items[i].data, cache->files.items[i].size);
+			ret[cache->files.items[i].size] = 0;
 			logr(debug, "Retrieving file %s\n", path);
 			return ret;
 		}
@@ -55,10 +54,10 @@ void *cache_load(const struct file_cache *cache, const char *path, size_t *lengt
 
 char *cache_encode(const struct file_cache *cache) {
 	cJSON *fileCache = cJSON_CreateArray();
-	for (size_t i = 0; i < cache->file_count; ++i) {
+	for (size_t i = 0; i < cache->files.count; ++i) {
 		cJSON *record = cJSON_CreateObject();
-		char *encoded = b64encode(cache->files[i].data, cache->files[i].size);
-		cJSON_AddStringToObject(record, "path", cache->files[i].path);
+		char *encoded = b64encode(cache->files.items[i].data, cache->files.items[i].size);
+		cJSON_AddStringToObject(record, "path", cache->files.items[i].path);
 		cJSON_AddStringToObject(record, "data", encoded);
 		free(encoded);
 		cJSON_AddItemToArray(fileCache, record);
@@ -84,12 +83,10 @@ void cache_decode(struct file_cache *cache, const char *data) {
 }
 
 void cache_destroy(struct file_cache *cache) {
-	for (size_t i = 0; i < cache->file_count; ++i) {
-		if (cache->files[i].data) free(cache->files[i].data);
-		if (cache->files[i].path) free(cache->files[i].path);
+	for (size_t i = 0; i < cache->files.count; ++i) {
+		if (cache->files.items[i].data) free(cache->files.items[i].data);
+		if (cache->files.items[i].path) free(cache->files.items[i].path);
 	}
-	free(cache->files);
-	cache->files = NULL;
-	cache->file_count = 0;
+	file_arr_free(&cache->files);
 	logr(debug, "Destroyed cache\n");
 }
--- src/utils/filecache.h
+++ src/utils/filecache.h
@@ -1,15 +1,16 @@
 //
 //  filecache.h
-//  C-Ray
+//  c-ray
 //
 //  Created by Valtteri Koskivuori on 29/03/2021.
-//  Copyright © 2021 Valtteri Koskivuori. All rights reserved.
+//  Copyright © 2021-2023 Valtteri Koskivuori. All rights reserved.
 //
 
 #pragma once
 
 #include <sys/types.h>
 #include <stdbool.h>
+#include "dyn_array.h"
 
 struct file {
 	char *path;
@@ -17,19 +18,16 @@ struct file {
 	void *data;
 };
 
+typedef struct file file;
+dyn_array_def(file);
+
 struct file_cache {
-	size_t file_count;
-	struct file *files;
+	struct file_arr files;
 };
 
 bool cache_contains(const struct file_cache *cache, const char *path);
-
 void cache_store(struct file_cache *cache, const char *path, const void *data, size_t length);
-
 void *cache_load(const struct file_cache *cache, const char *path, size_t *length);
-
 char *cache_encode(const struct file_cache *cache);
-
 void cache_decode(struct file_cache *cache, const char *data);
-
 void cache_destroy(struct file_cache *cache);
