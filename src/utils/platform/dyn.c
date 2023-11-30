b8ea67813d1b51154ae4c1b578061fea1240e12f
Valtteri Koskivuori <vkoskiv@gmail.com>
2023-11-30 23:09:49 +0200

    Add cosmopolitan cosmo_dl*() support
    It seems to mostly work to load SDL2 at runtime. There is still some
    weirdness with anything higher than -O0, though.

--- src/utils/platform/dyn.c
+++ src/utils/platform/dyn.c
@@ -1,22 +1,30 @@
-#ifdef WINDOWS
-#include <windows.h>
-#include <libloaderapi.h>
+#if defined(WINDOWS)
+	#include <windows.h>
+	#include <libloaderapi.h>
+#elif defined(__COSMOPOLITAN__)
+	#define _COSMO_SOURCE
+	#include "libc/dlopen/dlfcn.h"
 #else
-#include <dlfcn.h>
+	#include <dlfcn.h>
 #endif
+
 #include "dyn.h"
 
 void *dyn_load(const char *filename) {
-#ifdef WINDOWS
+#if defined(WINDOWS)
 	return (void *)LoadLibraryA(filename);
+#elif defined(__COSMOPOLITAN__)
+	return cosmo_dlopen(filename, RTLD_LAZY);
 #else
 	return dlopen(filename, RTLD_LAZY);
 #endif
 }
 
 void *dyn_sym(void *handle, const char *name) {
-#ifdef WINDOWS
+#if defined(WINDOWS)
 	return (void *)GetProcAddress((HMODULE)handle, name);
+#elif defined(__COSMOPOLITAN__)
+	return cosmo_dlsym(handle, name);
 #else
 	return dlsym(handle, name);
 #endif
@@ -25,14 +33,18 @@ void *dyn_sym(void *handle, const char *name) {
 char *dyn_error(void) {
 #if defined(WINDOWS)
 	return NULL;
+#elif defined(__COSMOPOLITAN__)
+	return cosmo_dlerror();
 #else
 	return dlerror();
 #endif
 }
 
 int dyn_close(void *handle) {
-#ifdef WINDOWS
+#if defined(WINDOWS)
 	return (int)FreeLibrary((HMODULE)handle);
+#elif defined(__COSMOPOLITAN__)
+	return cosmo_dlclose(handle);
 #else
 	return dlclose(handle);
 #endif
