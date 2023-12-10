7e70c73f3c195d68a546fbced6ff9a8edc1c4053
Valtteri Koskivuori <vkoskiv@gmail.com>
2023-12-10 20:14:12 +0200

    python: Statically link libc-ray in cray_wrap.so
    Since I'm going to be moving things around a lot while working on the
    Blender plugin, having this .so point to another is really painful,
    since apparently the $ORIGIN string for rpath isn't supported by all
    dynamic linkers. Fun.

--- lib.mk
+++ lib.mk
@@ -32,6 +32,6 @@ $(LIB): $(OBJS_lib)
 $(BIN_lib): $(LIB) $(OBJS_driver) $(OBJDIR_driver)
 	@echo "LD $@"
 	@$(CC) $(CFLAGS) $(OBJS_driver) $(LIB) -o $@ $(LDFLAGS)
-wrappers/cray_wrap.so: $(LIB) wrappers/cray_wrap.c
+wrappers/cray_wrap.so: $(OBJS_lib) wrappers/cray_wrap.c
 	@echo "Building Python module"
-	@$(CC) -shared $(CFLAGS) -fPIC `pkg-config --cflags python3` wrappers/cray_wrap.c $(LIB) -o $@
+	@$(CC) -shared $(CFLAGS) -fPIC `pkg-config --cflags python3` wrappers/cray_wrap.c $(OBJS_lib) -o $@
