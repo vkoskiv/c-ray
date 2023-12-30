ca788c74056b5333460259385f6c0b8c0e7dbc99
Valtteri Koskivuori <vkoskiv@gmail.com>
2023-12-30 16:55:20 +0200

    blender: Enable material UI
    Again just copying the Cycles equivalent to let Blender know it's okay
    to display that. I'm not sure if there is a shorter way to do this that
    wouldn't require copying this boilerplate code.

--- bindings/blender_ui.py
+++ bindings/blender_ui.py
@@ -93,6 +93,67 @@ class C_RAY_RENDER_PT_light_paths_bounces(CrayButtonsPanel, Panel):
 		col = layout.column()
 		col.prop(context.scene.c_ray, "bounces", text="Total")
 
+class CRAY_PT_context_material(CrayButtonsPanel, Panel):
+    bl_label = ""
+    bl_context = "material"
+    bl_options = {'HIDE_HEADER'}
+
+    @classmethod
+    def poll(cls, context):
+        if context.active_object and context.active_object.type == 'GPENCIL':
+            return False
+        else:
+            return (context.material or context.object) and CrayButtonsPanel.poll(context)
+
+    def draw(self, context):
+        layout = self.layout
+
+        mat = context.material
+        ob = context.object
+        slot = context.material_slot
+        space = context.space_data
+
+        if ob:
+            is_sortable = len(ob.material_slots) > 1
+            rows = 3
+            if (is_sortable):
+                rows = 4
+
+            row = layout.row()
+
+            row.template_list("MATERIAL_UL_matslots", "", ob, "material_slots", ob, "active_material_index", rows=rows)
+
+            col = row.column(align=True)
+            col.operator("object.material_slot_add", icon='ADD', text="")
+            col.operator("object.material_slot_remove", icon='REMOVE', text="")
+            col.separator()
+            col.menu("MATERIAL_MT_context_menu", icon='DOWNARROW_HLT', text="")
+
+            if is_sortable:
+                col.separator()
+
+                col.operator("object.material_slot_move", icon='TRIA_UP', text="").direction = 'UP'
+                col.operator("object.material_slot_move", icon='TRIA_DOWN', text="").direction = 'DOWN'
+
+            if ob.mode == 'EDIT':
+                row = layout.row(align=True)
+                row.operator("object.material_slot_assign", text="Assign")
+                row.operator("object.material_slot_select", text="Select")
+                row.operator("object.material_slot_deselect", text="Deselect")
+
+        row = layout.row()
+
+        if ob:
+            row.template_ID(ob, "active_material", new="material.new")
+
+            if slot:
+                icon_link = 'MESH_DATA' if slot.link == 'DATA' else 'OBJECT_DATA'
+                row.prop(slot, "link", text="", icon=icon_link, icon_only=True)
+
+        elif mat:
+            layout.template_ID(space, "pin_id")
+            layout.separator()
+
 class C_RAY_MATERIAL_PT_preview(CrayButtonsPanel, Panel):
 	bl_label = "Preview"
 	bl_context = "material"
@@ -180,6 +241,7 @@ classes = (
 	C_RAY_RENDER_PT_performance_clustering,
 	C_RAY_RENDER_PT_light_paths,
 	C_RAY_RENDER_PT_light_paths_bounces,
+	CRAY_PT_context_material,
 	C_RAY_MATERIAL_PT_preview,
 	C_RAY_CAMERA_PT_dof,
 	C_RAY_CAMERA_PT_dof_aperture,
