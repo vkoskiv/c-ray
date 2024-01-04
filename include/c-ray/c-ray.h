vkoskiv/c-ray

commit c0a6b2d4a20abf612c8af2886915ea5e28561327

lib+blender: Adjust render buffer size in renderer
It's extremely hacky, the whole renderer really isn't built with
interactivity in mind, so I'm just patching things all over the place at
runtime to keep the thing going. I think I need a bigger re-think of the
core stuff.

src/lib/renderer/renderer.c
@@ -209,7 +209,7 @@ void renderer_render(struct renderer *r) {
 		tex_clear(r->state.result_buf);
 	}
 
-	struct texture *result = r->state.result_buf;
+	struct texture **result = &r->state.result_buf;
 
 	struct cr_tile *info_tiles = calloc(set.tiles.count, sizeof(*info_tiles));
 	struct cr_renderer_cb_info cb_info = {
@@ -309,8 +309,9 @@ void renderer_render(struct renderer *r) {
 void *render_thread_interactive(void *arg) {
 	block_signals();
 	struct worker *threadState = (struct worker*)thread_user_data(arg);
+	threadState->in_pause_loop = false;
 	struct renderer *r = threadState->renderer;
-	struct texture *buf = threadState->buf;
+	struct texture **buf = threadState->buf;
 	sampler *sampler = newSampler();
 
 	struct camera *cam = threadState->cam;
@@ -328,13 +329,13 @@ void *render_thread_interactive(void *arg) {
 		for (int y = tile->end.y - 1; y > tile->begin.y - 1; --y) {
 			for (int x = tile->begin.x; x < tile->end.x; ++x) {
 				if (r->state.render_aborted) goto exit;
-				uint32_t pixIdx = (uint32_t)(y * buf->width + x);
+				uint32_t pixIdx = (uint32_t)(y * (*buf)->width + x);
 				//FIXME: This does not converge to the same result as with regular renderThread.
 				//I assume that's because we'd have to init the sampler differently when we render all
 				//the tiles in one go per sample, instead of the other way around.
 				initSampler(sampler, SAMPLING_STRATEGY, r->state.finishedPasses, r->prefs.sampleCount, pixIdx);
 				
-				struct color output = textureGetPixel(buf, x, y, false);
+				struct color output = textureGetPixel(*buf, x, y, false);
 				struct color sample = path_trace(cam_get_ray(cam, x, y, sampler), r->scene, r->prefs.bounces, sampler);
 
 				nan_clamp(&sample, &output);
@@ -346,22 +347,26 @@ void *render_thread_interactive(void *arg) {
 				output = colorCoef(t, output);
 				
 				//Store internal render buffer (float precision)
-				setPixel(buf, output, x, y);
+				setPixel(*buf, output, x, y);
 			}
 		}
 		//For performance metrics
 		total_us += timer_get_us(timer);
 		threadState->totalSamples++;
-		//Pause rendering when bool is set
-		while (threadState->paused && !r->state.render_aborted) {
-			timer_sleep_ms(100);
-		}
 		threadState->avg_per_sample_us = total_us / r->state.finishedPasses;
 		
 		//Tile has finished rendering, get a new one and start rendering it.
 		tile->state = finished;
 		threadState->currentTile = NULL;
 		tile = tile_next_interactive(r, threadState->tiles);
+		//Pause rendering when bool is set
+		while (threadState->paused && !r->state.render_aborted) {
+			threadState->in_pause_loop = true;
+			timer_sleep_ms(100);
+		}
+		threadState->in_pause_loop = false;
+		// In case we got NULL back because we were paused:
+		if (!tile) tile = tile_next_interactive(r, threadState->tiles);
 		threadState->currentTile = tile;
 	}
 exit:
@@ -382,7 +387,7 @@ void *render_thread(void *arg) {
 	block_signals();
 	struct worker *threadState = (struct worker*)thread_user_data(arg);
 	struct renderer *r = threadState->renderer;
-	struct texture *buf = threadState->buf;
+	struct texture **buf = threadState->buf;
 	sampler *sampler = newSampler();
 
 	struct camera *cam = threadState->cam;
@@ -402,10 +407,10 @@ void *render_thread(void *arg) {
 			for (int y = tile->end.y - 1; y > tile->begin.y - 1; --y) {
 				for (int x = tile->begin.x; x < tile->end.x; ++x) {
 					if (r->state.render_aborted) goto exit;
-					uint32_t pixIdx = (uint32_t)(y * buf->width + x);
+					uint32_t pixIdx = (uint32_t)(y * (*buf)->width + x);
 					initSampler(sampler, SAMPLING_STRATEGY, samples - 1, r->prefs.sampleCount, pixIdx);
 					
-					struct color output = textureGetPixel(buf, x, y, false);
+					struct color output = textureGetPixel(*buf, x, y, false);
 					struct color sample = path_trace(cam_get_ray(cam, x, y, sampler), r->scene, r->prefs.bounces, sampler);
 					
 					// Clamp out fireflies - This is probably not a good way to do that.
@@ -418,7 +423,7 @@ void *render_thread(void *arg) {
 					output = colorCoef(t, output);
 					
 					//Store internal render buffer (float precision)
-					setPixel(buf, output, x, y);
+					setPixel(*buf, output, x, y);
 				}
 			}
 			//For performance metrics

bindings/blender_init.py
@@ -168,6 +168,23 @@ def on_status_update(renderer_cb_info, args):
 		print("Stopping c-ray")
 		cr_renderer.stop()
 
+def draw_direct(bitmap):
+	if not bitmap:
+		return
+	float_count = bitmap.width * bitmap.height * bitmap.stride
+	buffer_from_memory = ct.pythonapi.PyMemoryView_FromMemory
+	buffer_from_memory.restype = ct.py_object
+	buffer = buffer_from_memory(bitmap.data.float_ptr, 4 * float_count)
+	pixels = np.frombuffer(buffer, np.float32)
+	pixels = gpu.types.Buffer('FLOAT', float_count, pixels)
+	texture = None
+	try:
+		texture = gpu.types.GPUTexture((bitmap.width, bitmap.height), format='RGBA32F', data=pixels)
+	except ValueError as error:
+		print("Texture creation didn't work: {} (width: {}, height: {})".format(error, bitmap.width, bitmap.height))
+	if texture:
+		draw_texture_2d(texture, (0, 0), texture.width, texture.height)
+
 def status_update_interactive(renderer_cb_info, args):
 	tag_redraw, update_stats, self = args
 	ct.pythonapi.PyCapsule_GetPointer.restype = ct.c_void_p
@@ -188,11 +205,11 @@ class CrayRender(bpy.types.RenderEngine):
 	cr_interactive_running = False
 	cr_renderer = None
 	old_mtx = None
+	old_dims = None
 
 	def __init__(self):
 		print("c-ray initialized")
 		self.cr_scene = None
-		self.draw_data = None
 		self.cr_interactive_running = False
 
 	def __del__(self):
@@ -349,18 +366,16 @@ class CrayRender(bpy.types.RenderEngine):
 		if not self.old_mtx or mtx != self.old_mtx:
 			self.tag_update()
 		self.old_mtx = mtx
-		dimensions = (context.region.width, context.region.height)
+		new_dims = (context.region.width, context.region.height)
+		if not self.old_dims or self.old_dims != new_dims:
+			cr_cam = self.cr_scene.cameras['Camera']
+			cr_cam.set_param(c_ray.cam_param.res_x, context.region.width)
+			cr_cam.set_param(c_ray.cam_param.res_y, context.region.height)
+			self.cr_renderer.restart()
+			self.old_dims = new_dims
 		gpu.state.blend_set('ALPHA_PREMULT')
 		self.bind_display_space_shader(depsgraph.scene)
-		if not self.draw_data or self.draw_data.dimensions != dimensions:
-			print("Regenerating draw_data")
-			bm = self.cr_renderer.get_result()
-			if not bm:
-				print("No bitmap yet")
-				return
-			self.draw_data = CrayDrawData(dimensions, bm)
-
-		self.draw_data.draw()
+		draw_direct(self.cr_renderer.get_result())
 		self.unbind_display_space_shader()
 		gpu.state.blend_set('NONE')
 
@@ -430,38 +445,6 @@ class CrayRender(bpy.types.RenderEngine):
 		print("Displaying bitmap took a total of {}s".format(end - start_first))
 
 		self.end_result(result)
-
-class CrayDrawData:
-	pixels = None
-	def __init__(self, dimensions, bitmap):
-		self.dimensions = dimensions
-		self.bitmap = bitmap
-		width, height = self.dimensions
-
-		float_count = self.bitmap.width * self.bitmap.height * self.bitmap.stride
-		buffer_from_memory = ct.pythonapi.PyMemoryView_FromMemory
-		buffer_from_memory.restype = ct.py_object
-		buffer = buffer_from_memory(self.bitmap.data.float_ptr, 4 * float_count)
-		pixels = np.frombuffer(buffer, np.float32)
-		self.pixels = gpu.types.Buffer('FLOAT', float_count, pixels)
-
-	def __del__(self):
-		try:
-			del self.pixels
-		except AttributeError:
-			print("No self.pixels")
-
-	def draw(self):
-		# FIXME: I have no idea how to create a GPUTexture that points to my raw float array on the C side.
-		# So for now, just generate a new texture from the data on every redraw instead.
-		width, height = self.dimensions
-		texture = None
-		try:
-			texture = gpu.types.GPUTexture((width, height), format='RGBA32F', data=self.pixels)
-		except ValueError:
-			print("Texture creation didn't work. width: {}, height: {}".format(width, height))
-		if texture:
-			draw_texture_2d(texture, (0, 0), texture.width, texture.height)
 
 def register():
 	from . import properties

src/lib/api/c-ray.c
@@ -750,6 +750,22 @@ void cr_renderer_restart_interactive(struct cr_renderer *ext) {
 		logr(warning, "restart: No tile set, bailing\n");
 		return;
 	}
+	struct camera *cam = &r->scene->cameras.items[r->prefs.selected_camera];
+	if (r->state.result_buf->width != (size_t)cam->width || r->state.result_buf->height != (size_t)cam->height) {
+		// Resize result buffer. First, pause render threads and wait for them to ack
+		logr(info, "Resizing result_buf (%zu,%zu) -> (%d,%d)\n", r->state.result_buf->width, r->state.result_buf->height, cam->width, cam->height);
+		cr_renderer_toggle_pause((struct cr_renderer *)r);
+		for (size_t i = 0; i < r->state.workers.count; ++i) {
+			while (!r->state.workers.items[i].in_pause_loop) {
+				timer_sleep_ms(1);
+			}
+		}
+		// Okay, threads are now paused, swap the buffer
+		destroyTexture(r->state.result_buf);
+		r->state.result_buf = newTexture(float_p, cam->width, cam->height, 4);
+
+		cr_renderer_toggle_pause((struct cr_renderer *)r);
+	}
 	// sus
 	r->state.finishedPasses = 1;
 	mutex_lock(r->state.current_set->tile_mutex);

src/lib/datatypes/tile.c
@@ -60,7 +60,8 @@ struct render_tile *tile_next_interactive(struct renderer *r, struct tile_set *s
 		}
 	}
 	if (!tile) {
-		if (r->state.render_aborted) {
+		// FIXME: shared state to indicate pause instead of accessing worker state
+		if (r->state.render_aborted || r->state.workers.items[0].paused) {
 			mutex_release(set->tile_mutex);
 			return NULL;
 		}

src/lib/protocol/server.c
@@ -156,7 +156,7 @@ static cJSON *handle_submit_work(struct worker *state, const cJSON *json) {
 	for (int y = tile.end.y - 1; y > tile.begin.y - 1; --y) {
 		for (int x = tile.begin.x; x < tile.end.x; ++x) {
 			struct color value = textureGetPixel(texture, x - tile.begin.x, y - tile.begin.y, false);
-			setPixel(state->buf, value, x, y);
+			setPixel(*state->buf, value, x, y);
 		}
 	}
 	destroyTexture(texture);

src/lib/renderer/renderer.h
@@ -19,6 +19,7 @@ struct worker {
 	bool thread_complete;
 	
 	bool paused; //SDL listens for P key pressed, which sets these, one for each thread.
+	bool in_pause_loop;
 	
 	//Share info about the current tile with main thread
 	struct tile_set *tiles;
@@ -29,7 +30,7 @@ struct worker {
 
 	struct camera *cam;
 	struct renderer *renderer;
-	struct texture *buf;
+	struct texture **buf;
 	struct render_client *client; // Optional
 };
 typedef struct worker worker;

commit 7ede985420a89ab80eca253a5e27de3c4dd08384

lib: Fix bug introduced in c0a6b2d4
I wasn't passing the render buffer to the driver callbacks properly, and
that was hidden by the casts that happen in the private/public boundary.

include/c-ray/c-ray.h
@@ -86,7 +86,7 @@ struct cr_tile {
 };
 
 struct cr_renderer_cb_info {
-	const struct cr_bitmap *fb;
+	const struct cr_bitmap **fb;
 	const struct cr_tile *tiles;
 	size_t tiles_count;
 

src/driver/main.c
@@ -30,7 +30,7 @@ struct usr_data {
 
 static void on_start(struct cr_renderer_cb_info *info, void *user_data) {
 	struct usr_data *d = user_data;
-	if (d->p.enabled && info->fb) d->w = win_try_init(&d->p, info->fb->width, info->fb->height);
+	if (d->p.enabled && *info->fb) d->w = win_try_init(&d->p, (*info->fb)->width, (*info->fb)->height);
 }
 
 static void on_stop(struct cr_renderer_cb_info *info, void *user_data) {
@@ -43,7 +43,7 @@ static void status(struct cr_renderer_cb_info *state, void *user_data) {
 	static int pauser = 0;
 	struct usr_data *d = user_data;
 	if (!d) return;
-	struct input_state in = win_update(d->w, state->tiles, state->tiles_count, (struct texture *)state->fb);
+	struct input_state in = win_update(d->w, state->tiles, state->tiles_count, *((struct texture **)state->fb));
 	d->should_save = in.should_save;
 	if (in.stop_render) cr_renderer_stop(d->r);
 	if (in.pause_render) cr_renderer_toggle_pause(d->r);

src/lib/renderer/renderer.c
@@ -215,7 +215,7 @@ void renderer_render(struct renderer *r) {
 	struct cr_renderer_cb_info cb_info = {
 		.tiles = info_tiles,
 		.tiles_count = set.tiles.count,
-		.fb = (struct cr_bitmap *)result,
+		.fb = (const struct cr_bitmap **)result,
 	};
 	
 	struct callback start = r->state.callbacks[cr_cb_on_start];