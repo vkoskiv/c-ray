vkoskiv/c-ray

commit 3f7569e437832c67a2e84cff10aa960e4b4a659f

lib+driver+common: Restructure #include statements
Previously, we were using file-relative quote includes for everything.
This was okay in the beginning, but as different components were
separated out into subdirectories, include statements became quite an
unpleasant mess of "../../.." to hold everything together.
Since src/ is now (mostly) neatly separated out to src/lib for the
renderer proper, src/driver for the old bin/c-ray driver program for
I/O, and src/common for code shared between the two, I now decided to
add new include paths in the build to facilitate neater angle-bracket
includes throughout.
Each subdirectory under src/ now has -I./src/<subdir>/, so code under
the subdirectory can #include <> relative to its root. We also add
-I./src/ for cross-subdirectory includes, so code in, src/lib or
src/driver can both #include <common/vector.h>, for example.
If it isn't clear, the intent is for src/lib and src/driver to both
depend on src/common, but src/common shouldn't include anything from
src/lib or src/driver, and src/lib also shouldn't include anything from
src/driver, and vice versa. I can't currently come up with a way to
enforce this, but it should be easy enough to avoid.

To facilitate this change, I had to rename a few files to avoid
colliding with system headers:
- assert.[c,h] -> cr_assert.[c,h]
- string.[c,h] -> cr_string.[c,h]

src/common/networking.c
@@ -1,9 +1,9 @@
 //
 //  networking.c
-//  C-ray
+//  c-ray
 //
 //  Created by Valtteri on 5.1.2020.
-//  Copyright © 2020-2022 Valtteri Koskivuori. All rights reserved.
+//  Copyright © 2020-2025 Valtteri Koskivuori. All rights reserved.
 //
 
 #include "../includes.h"
@@ -22,7 +22,7 @@
 #include <fcntl.h>
 #include <arpa/inet.h>
 #include <string.h>
-#include "assert.h"
+#include "cr_assert.h"
 #include <errno.h>
 #include "fileio.h"
 

commit 72b89ec82943989c79c8130bd5b8b25dfc0a2aeb

common: Try to fix htonll/ntohll on macOS
I haven't actively tested on macOS since ~2021, not entirely sure why
this broke in 3f7569e, but this is a better approach anyway.

src/common/networking.c
@@ -26,19 +26,19 @@
 #include <errno.h>
 #include "fileio.h"
 
-#ifndef __APPLE__
-	#if __BIG_ENDIAN__
-		#define htonll(x)   (x)
-		#define ntohll(x)   (x)
-	#else
-		#define htonll(x)   ((((uint64_t)htonl(x & 0xFFFFFFFF)) << 32) + htonl(x >> 32))
-		#define ntohll(x)   ((((uint64_t)ntohl(x & 0xFFFFFFFF)) << 32) + ntohl(x >> 32))
+#if __BYTE_ORDER__ == __BIG_ENDIAN
+	#ifndef ntohll
+		#define ntohll(x) (x)
+	#endif
+	#ifndef htonll
+		#define htonll(x) (x)
 	#endif
 #else
-	// PowerPC Mac, probably.
-	#if __BIG_ENDIAN__
-		#define htonll(x)   (x)
-		#define ntohll(x)   (x)
+	#ifndef htonll
+		#define htonll(x)   ((((uint64_t)htonl(x & 0xFFFFFFFF)) << 32) + htonl(x >> 32))
+	#endif
+	#ifndef ntohll
+		#define ntohll(x)   ((((uint64_t)ntohl(x & 0xFFFFFFFF)) << 32) + ntohl(x >> 32))
 	#endif
 #endif
 