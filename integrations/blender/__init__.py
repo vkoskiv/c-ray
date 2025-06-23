vkoskiv/c-ray

commit 1114378d4d7dd7ecdf5ed71d6ff7d57c03ac98c9

blender: Fix bug where c-ray was never deleted
Turns out, I was passing self into the callback args, which presumably
then held a reference to it, which meant that the Blender python runtime
never calls __del__() on it. I wasn't even using self for anything, I
was just grabbing the current sample count from it.
Fix is to just pass in the sample count directly, instead of passing in
self.

integrations/blender/__init__.py
@@ -171,9 +171,9 @@ def draw_direct(bitmap):
 		draw_texture_2d(texture, (0, 0), texture.width, texture.height)
 
 def status_update_interactive(cb_info, args):
-	tag_redraw, update_stats, self = args
+	tag_redraw, update_stats, total_samples = args;
 	tag_redraw()
-	if cb_info.finished_passes == self.cr_renderer.prefs.samples:
+	if cb_info.finished_passes == total_samples:
 		update_stats("Rendering done", "")
 	else:
 		update_stats("Sample {}".format(cb_info.finished_passes), "")
@@ -194,8 +194,6 @@ class CrayRender(bpy.types.RenderEngine):
 		c_ray.log_level_set(c_ray.log_level.Debug)
 
 	def __del__(self):
-		# FIXME: This never gets called when leaving rendered viewport shading mode
-		# which ends up leaving zombie instances of c-ray running in the background
 		if self.cr_renderer:
 			if self.cr_renderer.interactive:
 				self.cr_renderer.stop()
@@ -482,7 +480,7 @@ class CrayRender(bpy.types.RenderEngine):
 			sync_end = time.time()
 			print("Sync took {}s".format(sync_end - sync_start))
 			print("Kicking off background renderer")
-			self.cr_renderer.callbacks.on_interactive_pass_finished = (status_update_interactive, (self.tag_redraw, self.update_stats, self))
+			self.cr_renderer.callbacks.on_interactive_pass_finished = (status_update_interactive, (self.tag_redraw, self.update_stats, self.cr_renderer.prefs.samples))
 			self.cr_renderer.start_interactive()
 
 	def display_bitmap(self, bm):

commit fb9c603a5e2ef2f7ca725c2854205b0b693e2ccc

blender: Fix render status text regression
In 1114378d I changed interactive mode status update callback to take
the sample count value directly. This works, but the logic to display
'Rendering done' no longer works because it's just using whatever value
was passed in for sample count at init time. So if I start interactive
mode at 16 samples, then later bump the sample limit to 32, it
incorrectly displays 'Rendering done' instead of 'Sample 16' when the
pass count hits 16.
Now I pass in cr_renderer.prefs instead, so status_update_interactive()
can grab the current value for sample count.

integrations/blender/__init__.py
@@ -178,9 +178,9 @@ def draw_direct(bitmap):
 		draw_texture_2d(texture, (0, 0), texture.width, texture.height)
 
 def status_update_interactive(cb_info, args):
-	tag_redraw, update_stats, total_samples = args;
+	tag_redraw, update_stats, prefs = args;
 	tag_redraw()
-	if cb_info.finished_passes == total_samples:
+	if cb_info.finished_passes == prefs.samples:
 		update_stats("Rendering done", "")
 	else:
 		update_stats("Sample {}".format(cb_info.finished_passes), "")
@@ -472,7 +472,7 @@ class CrayRender(bpy.types.RenderEngine):
 			sync_end = time.time()
 			print("Sync took {}s".format(sync_end - sync_start))
 			print("Kicking off background renderer")
-			self.cr_renderer.callbacks.on_interactive_pass_finished = (status_update_interactive, (self.tag_redraw, self.update_stats, self.cr_renderer.prefs.samples))
+			self.cr_renderer.callbacks.on_interactive_pass_finished = (status_update_interactive, (self.tag_redraw, self.update_stats, self.cr_renderer.prefs))
 			self.cr_renderer.start_interactive()
 
 	def display_bitmap(self, bm):